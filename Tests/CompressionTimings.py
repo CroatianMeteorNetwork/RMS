f26fe0072414b7e74ce650256313f9cf12ca6d93
Dario Zubović <dzubovic@aad.hr>
2015-05-18 21:22:30 +0200

    addition of conversion into compression timing and results of running test

--- Tests/CompressionTimings.py
+++ Tests/CompressionTimings.py
@@ -14,26 +14,48 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+""" Timings of compression algorithm with various cases.
+
+Averaged results (20 runs) for Raspberry Pi 2 at xxghz:
+black: 10.0018053055
+white: 9.9432981679
+uniform noise: 5.16999864578
+Gaussian noise: 5.31156075001
+"""
+
 from RMS.Compression import Compression
 import numpy as np
 import time
 
 def timing(img, s):
     t = time.time()
+    img = Compression.convert(img)
     Compression.compress(img)
     print s + ": " + str(time.time() - t) + "s"
+   
+def create(f):
+    arr = np.empty((256, 576, 720), np.uint8)
+    for i in range(256):
+        arr[i] = f()
+    return arr
+
+def black():
+    return np.zeros((576, 720), np.uint8)
+
+def white():
+    return np.full((576, 720), 255, np.uint8)
+
+def uniform():
+    return np.random.uniform(0, 256, (576, 720))
+
+def gauss():
+    return np.random.normal(128, 2, (576, 720))
 
 if __name__ == "__main__":
-    shape = (576, 720, 256)
-    
-    black = np.zeros(shape, np.uint8)
-    timing(black, "black")
-    
-    white = np.full(shape, 255, np.uint8)
-    timing(white, "white")
-    
-    noise = np.random.uniform(0, 256, shape)
-    timing(noise, "uniform noise")
-    
-    gauss = np.random.normal(128, 2, shape)
-    timing(gauss, "Gaussian noise")
+    timing(create(black), "black")
+        
+    timing(create(white), "white")
+        
+    timing(create(uniform), "uniform noise")
+        
+    timing(create(gauss), "Gaussian noise")
