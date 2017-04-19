b3d7eb9b659f28ca5d52de0752a2abae1e4b177b
denis.vida <denis.vida@gmail.com>
2017-04-19 10:35:25 -0400

    changed archive compression to BZ2, as ZIP files are limited to 2 GB

--- RMS/Misc.py
+++ RMS/Misc.py
@@ -2,6 +2,10 @@
 import os
 import shutil
 import errno
+import logging
+
+# Get the logger from the main module
+log = logging.getLogger("logger")
 
 
 def mkdirP(path):
@@ -23,18 +27,19 @@ def mkdirP(path):
 
 
 
-def archiveDir(source_dir, file_list, dest_dir, zipname, delete_dest_dir=False):
-    """ Move the given file list from the source directory to the destination directory, ZIP the destination 
-        directory and save it as a .zip file.
+def archiveDir(source_dir, file_list, dest_dir, compress_file, delete_dest_dir=False):
+    """ Move the given file list from the source directory to the destination directory, compress the 
+        destination directory and save it as a .bz2 file. BZ2 compression is used as ZIP files have a limit
+        of 2GB in size.
 
     Arguments:
         source_dir: [str] Path to the directory from which the files will be taken and archived.
         file_list: [list] A list of files from the source_dir which will be archived.
-        dest_dir: [str] Path to the archive directory which will be zipped.
-        zipname: [str] Name of the zip file which will be created.
+        dest_dir: [str] Path to the archive directory which will be compressed.
+        compress_file: [str] Name of the compressed file which will be created.
 
     Keyword arguments:
-        delete_dest_dir: [bool] Delete the destination directory after zipping. False by defualt.
+        delete_dest_dir: [bool] Delete the destination directory after compression. False by defualt.
 
     """
 
@@ -45,9 +50,9 @@ def archiveDir(source_dir, file_list, dest_dir, zipname, delete_dest_dir=False):
     for file_name in file_list:
         shutil.copy2(os.path.join(source_dir, file_name), os.path.join(dest_dir, file_name))
 
-    # Zip the archive directory
-    shutil.make_archive(os.path.join(dest_dir, zipname), 'zip', dest_dir)
+    # Compress the archive directory
+    shutil.make_archive(os.path.join(dest_dir, compress_file), 'bztar', dest_dir, logger=log)
 
-    # Delete the archive directory after zipping
+    # Delete the archive directory after compression
     if delete_dest_dir:
         shutil.rmtree(dest_dir)
