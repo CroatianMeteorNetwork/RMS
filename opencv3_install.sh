d4c9cfaa733190009931a6c3bd15f3ff87519da9
Denis Vida <denis.vida@gmail.com>
2017-11-14 00:11:08 -0500

    added gstreamer install

--- opencv3_install.sh
+++ opencv3_install.sh
@@ -9,6 +9,7 @@ sudo apt-get install -y build-essential cmake pkg-config
 sudo apt-get install -y libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev
 sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y gstreamer1.0
 sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 sudo apt-get install -y libgtk-3-dev
 sudo apt-get install -y libatlas-base-dev gfortran
