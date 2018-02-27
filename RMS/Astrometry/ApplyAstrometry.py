fixed astrometry, got rid of scaling to CIF

RMS/Astrometry/ApplyAstrometry.py
@@ -81,7 +81,7 @@ def calcRefCentre(JD, lon, lat, ra_ref, dec_ref):
 
 
 
-def applyFieldCorrection(x_poly, y_poly, X_res, Y_res, F_scale, X_data, Y_data, level_data):
+def applyFieldCorrection(x_poly, y_poly, X_res, Y_res, F_scale, X_data, Y_data):
     """ Apply field correction and vignetting correction to all given image points. 
 `
     Arguments:
@@ -92,39 +92,32 @@ def applyFieldCorrection(x_poly, y_poly, X_res, Y_res, F_scale, X_data, Y_data, 
         F_scale: [float] Sum of image scales per each image axis (arcsec per px).
         X_data: [ndarray] 1D float numpy array containing X component of the detection point.
         Y_data: [ndarray] 1D float numpy array containing Y component of the detection point.
-        level_data: [ndarray] 1D int numpy array containing levels of detection points.
     
     Return:
         (X_corrected, Y_corrected, levels_corrected): [tuple of ndarrays]
             X_corrected: 1D numpy array containing distortion corrected X component.
             Y_corrected: 1D numpy array containing distortion corrected Y component.
-            level_data: 1D numpy array containing vignetting corrected levels.
             
     """
 
-    # Scale the resolution to CIF
-    X_scale = X_res/384.0
-    Y_scale = Y_res/288.0
+    # # Scale the resolution to CIF
+    # X_scale = X_res/384.0
+    # Y_scale = Y_res/288.0
 
     # Initialize final values containers
     X_corrected = np.zeros_like(X_data, dtype=np.float64)
     Y_corrected = np.zeros_like(Y_data, dtype=np.float64)
-    levels_corrected = np.zeros_like(level_data, dtype=np.float64)
 
     i = 0
 
-    data_matrix = np.vstack((X_data, Y_data, level_data)).T
+    data_matrix = np.vstack((X_data, Y_data)).T
 
     # Go through all given data points
-    for Xdet, Ydet, level in data_matrix:
+    for Xdet, Ydet in data_matrix:
 
         # Scale the point coordinates to CIF resolution
-        Xdet = (Xdet - X_res/2)/X_scale
-        Ydet = (Ydet - Y_res/2)/Y_scale
-
-        # # Apply vignetting correction
-        # if (np.sqrt((Xdet - 192)**2 + (Ydet - 192)**2) > 120):
-        #     level = level * (1 + 0.00245*(np.sqrt((Xdet - 192)**2 + (Ydet - 192)**2) - 120))
+        Xdet = (Xdet - X_res/2.0)#/X_scale
+        Ydet = (Ydet - Y_res/2.0)#/Y_scale
 
         dX = (x_poly[0]
             + x_poly[1]*Xdet
@@ -165,11 +158,10 @@ def applyFieldCorrection(x_poly, y_poly, X_res, Y_res, F_scale, X_data, Y_data, 
         # Store values to final arrays
         X_corrected[i] = X_pix
         Y_corrected[i] = Y_pix
-        levels_corrected[i] = level
 
         i += 1
 
-    return X_corrected, Y_corrected, levels_corrected
+    return X_corrected, Y_corrected
 
 
 
@@ -285,12 +277,12 @@ def altAz2RADec(lat, lon, UT_corr, time_data, azimuth_data, altitude_data, dt_ti
     for azimuth, altitude in data_matrix:
 
         if dt_time:
-            JD = datetime2JD(time_data[i], UT_corr=UT_corr)
+            JD = datetime2JD(time_data[i], UT_corr=-UT_corr)
 
         else:
             # Extract time
             Y, M, D, h, m, s, ms = time_data[i]
-            JD = date2JD(Y, M, D, h, m, s, ms, UT_corr=UT_corr)
+            JD = date2JD(Y, M, D, h, m, s, ms, UT_corr=-UT_corr)
 
         # Convert altitude and azimuth to radians
         az_rad = math.radians(azimuth)
@@ -401,7 +393,7 @@ def calculateMagnitudes_old(level_data, ra_beg, ra_end, dec_beg, dec_end, durati
 
 
 
-def XY2CorrectedRADec(time_data, X_data, Y_data, level_data, UT_corr, lat, lon, Ho, X_res, Y_res, RA_d, dec_d, 
+def XY2CorrectedRADec(time_data, X_data, Y_data, level_data, lat, lon, Ho, X_res, Y_res, RA_d, dec_d, 
     pos_angle_ref, F_scale, mag_0, mag_lev, x_poly, y_poly):
     """ A function that does the complete calibration and coordinate transformations of a meteor detection.
 
@@ -415,7 +407,6 @@ def XY2CorrectedRADec(time_data, X_data, Y_data, level_data, UT_corr, lat, lon, 
         X_data: [ndarray] 1D numpy array containing the image X component.
         Y_data: [ndarray] 1D numpy array containing the image Y component.
         level_data: [ndarray] Levels of the meteor centroid.
-        UT_corr: [float] UT correction in hours (difference from local time to UT).
         lat: [float] Latitude of the observer in degrees.
         lon: [float] Longitde of the observer in degress.
         Ho: [float] Referent hour angle (deg).
@@ -440,17 +431,17 @@ def XY2CorrectedRADec(time_data, X_data, Y_data, level_data, UT_corr, lat, lon, 
     """
 
     # Apply field correction
-    X_corrected, Y_corrected, levels_corrected = applyFieldCorrection(x_poly, y_poly, X_res, Y_res, F_scale, \
-        X_data, Y_data, level_data)
+    X_corrected, Y_corrected = applyFieldCorrection(x_poly, y_poly, X_res, Y_res, F_scale, \
+        X_data, Y_data)
 
     # Convert XY image coordinates to azimuth and altitude
     az_data, alt_data = XY2altAz(lat, lon, RA_d, dec_d, Ho, pos_angle_ref, X_corrected, Y_corrected)
 
     # Convert azimuth and altitude data to right ascension and declination
-    JD_data, RA_data, dec_data = altAz2RADec(lat, lon, UT_corr, time_data, az_data, alt_data)
+    JD_data, RA_data, dec_data = altAz2RADec(lat, lon, 0, time_data, az_data, alt_data)
 
     # Calculate magnitudes
-    magnitude_data = calculateMagnitudes(levels_corrected, mag_0, mag_lev)
+    magnitude_data = calculateMagnitudes(level_data, mag_0, mag_lev)
 
 
     return JD_data, RA_data, dec_data, magnitude_data
@@ -479,7 +470,7 @@ def XY2CorrectedRADecPP(time_data, X_data, Y_data, level_data, platepar):
     """
 
 
-    return XY2CorrectedRADec(time_data, X_data, Y_data, level_data, platepar.UT_corr, platepar.lat, \
+    return XY2CorrectedRADec(time_data, X_data, Y_data, level_data, platepar.lat, \
         platepar.lon, platepar.Ho, platepar.X_res, platepar.Y_res, platepar.RA_d, platepar.dec_d, \
         platepar.pos_angle_ref, platepar.F_scale, platepar.mag_0, platepar.mag_lev, platepar.x_poly, \
         platepar.y_poly)
@@ -488,7 +479,7 @@ def XY2CorrectedRADecPP(time_data, X_data, Y_data, level_data, platepar):
 
 
 def raDecToCorrectedXY(RA_data, dec_data, jd, lat, lon, x_res, y_res, RA_d, dec_d, ref_jd, pos_angle_ref, \
-    F_scale, x_poly, y_poly):
+    F_scale, x_poly, y_poly, UT_corr=0):
     """ Convert RA, Dec to distorion corrected image coordinates. 
 
     Arguments:
@@ -506,6 +497,9 @@ def raDecToCorrectedXY(RA_data, dec_data, jd, lat, lon, x_res, y_res, RA_d, dec_
         F_scale: [float] Sum of image scales per each image axis (arcsec per px).
         x_poly: [ndarray float] Distorsion polynomial in X direction.
         y_poly: [ndarray float] Distorsion polynomail in Y direction.
+
+    Keyword arguments:
+        UT_corr: [float] UT correction (hours).
     
     Return:
         (x, y): [tuple of ndarrays] Image X and Y coordinates.
@@ -514,7 +508,9 @@ def raDecToCorrectedXY(RA_data, dec_data, jd, lat, lon, x_res, y_res, RA_d, dec_
     # Calculate the referent coordinates in azimuth and altitude
     az_centre, alt_centre = calcRefCentre(ref_jd, lon, lat, RA_d, dec_d)
 
-    
+    # Apply the UT correction
+    jd -= UT_corr/24.0
+
     # Use the cythonized funtion insted of the Python function
     return cyRaDecToCorrectedXY(RA_data, dec_data, jd, lat, lon, x_res, y_res, az_centre, alt_centre, 
         pos_angle_ref, F_scale, x_poly, y_poly)
@@ -529,7 +525,7 @@ def raDecToCorrectedXY(RA_data, dec_data, jd, lat, lon, x_res, y_res, RA_d, dec_
     
 
     # Calculate the referent hour angle
-    T = (jd - 2451545)/36525.0
+    T = (jd - 2451545.0)/36525.0
     Ho = 280.46061837 + 360.98564736629*(jd - 2451545) + 0.000387933*T**2 - (T**3)/38710000.0
 
     sl = math.sin(math.radians(lat))
@@ -584,7 +580,8 @@ def raDecToCorrectedXY(RA_data, dec_data, jd, lat, lon, x_res, y_res, RA_d, dec_
             + x_poly[11]*Y1*np.sqrt(X1**2 + Y1**2))
 
         # Add the distortion correction and calculate X image coordinates
-        Xpix = (X1 - dX)*x_res/384.0 + x_res/2
+        #Xpix = (X1 - dX)*x_res/384.0 + x_res/2
+        Xpix = X1 - dX + x_res/2.0
 
         # Calculate distortion in Y direction
         dY = (y_poly[0]
@@ -601,7 +598,8 @@ def raDecToCorrectedXY(RA_data, dec_data, jd, lat, lon, x_res, y_res, RA_d, dec_
             + y_poly[11]*X1*np.sqrt(X1**2 + Y1**2))
 
         # Add the distortion correction and calculate Y image coordinates
-        Ypix = (Y1 - dY)*y_res/288.0 + y_res/2
+        #Ypix = (Y1 - dY)*y_res/288.0 + y_res/2
+        Ypix = Y1 - dY + y_res/2.0
 
         x_array[i] = Xpix
         y_array[i] = Ypix
@@ -627,7 +625,7 @@ def raDecToCorrectedXYPP(RA_data, dec_data, jd, platepar):
 
     return raDecToCorrectedXY(RA_data, dec_data, jd, platepar.lat, platepar.lon, platepar.X_res, \
         platepar.Y_res, platepar.RA_d, platepar.dec_d, platepar.JD, platepar.pos_angle_ref, \
-        platepar.F_scale, platepar.x_poly, platepar.y_poly)
+        platepar.F_scale, platepar.x_poly, platepar.y_poly, UT_corr=platepar.UT_corr)
 
 
 
@@ -687,21 +685,21 @@ def applyAstrometryFTPdetectinfo(dir_path, ftp_detectinfo_file, platepar_file, U
             t = time_beg + datetime.timedelta(seconds=frame_n/fps)
             time_data.append([t.year, t.month, t.day, t.hour, t.minute, t.second, int(t.microsecond/1000)])
 
-        # Apply field correction
-        X_corrected, Y_corrected, levels_corrected = applyFieldCorrection(platepar.x_poly, platepar.y_poly, \
-            platepar.X_res, platepar.Y_res, platepar.F_scale, X_data, Y_data, level_data)
-
-        # Convert XY image coordinates to azimuth and altitude
-        az_data, alt_data = XY2altAz(platepar.lat, platepar.lon, platepar.RA_d, platepar.dec_d, platepar.Ho, \
-            platepar.pos_angle_ref, X_corrected, Y_corrected)
-
-        # Convert azimuth and altitude data to right ascension and declination
-        JD_data, RA_data, dec_data = altAz2RADec(platepar.lat, platepar.lon, UT_corr, time_data, az_data, 
-            alt_data)
 
 
-        ### CALCULATE MAGNITUDES
-        magnitudes = calculateMagnitudes(levels_corrected, platepar.mag_0, platepar.mag_lev)
+        # Convert image cooredinates to RA and Dec, and do the photometry
+        JD_data, RA_data, dec_data, magnitudes = XY2CorrectedRADecPP(np.array(time_data), X_data, Y_data, \
+            level_data, platepar)
+
+        # Do not calculate azimuth and altitude
+        az_data = np.zeros_like(RA_data)
+        alt_data = np.zeros_like(RA_data)
+
+
+        # print(ff_name, cam_code, meteor_No, fps)
+        # print(X_data, Y_data)
+        # print(RA_data, dec_data)
+        # print('------------------------------------------')
 
         # Construct the meteor measurements array
         meteor_picks = np.c_[frames, X_data, Y_data, RA_data, dec_data, az_data, alt_data, level_data, \
@@ -761,22 +759,40 @@ if __name__ == "__main__":
     print('Done!')
 
 
+
+    # sys.exit()
+
+
+    # # TEST CONVERSION FUNCTIONS
+
     # # Load the platepar
     # platepar = Platepar()
-    # platepar.read("C:\\Users\\delorayn1\\Desktop\\20170813_213506_620678\\platepar_cmn2010.cal")
+    # platepar.read("C:\Users\delorayn1\Desktop\HR0002_20180119_162419_928144_detected\platepar_cmn2010.cal")
 
-    # from RMS.Formats.FFfile import getMiddleTime
-    # time = getMiddleTime('FF100_20170813_213508_532_0000000.bin', 25)
+    # from RMS.Formats.FFfile import getMiddleTimeFF
+    # from RMS.Astrometry.Conversions import date2JD, jd2Date
+    # time = getMiddleTimeFF('FF_HR0002_20180119_210212_876_0414720.fits', 25)
 
-    # # Deneb
-    # star_x = 281.14
-    # star_y = 57.97
+    # # Convert time to UT
+    # #time = jd2Date(date2JD(*time, UT_corr=platepar.UT_corr))
 
-    # # # Vega
-    # # star_x = 546.27
-    # # star_y = 54.74
+    # # Sirius
+    # star_x = 619
+    # star_y = 401
 
-    # jd, ra, dec, mag = XY2CorrectedRADec([time], [star_x], [star_y], [0], 0, platepar.lat, platepar.lon, platepar.Ho, platepar.X_res, platepar.Y_res, platepar.RA_d, platepar.dec_d, platepar.pos_angle_ref, platepar.F_scale, platepar.w_pix, platepar.mag_0, platepar.mag_lev, platepar.x_poly, platepar.y_poly)
+
+    # ## TEST!!!
+    # ## OVERRIDE POLY 0th order
+    # # platepar.x_poly[0] = 0
+    # # platepar.y_poly[0] = 0
+    # # platepar.x_poly = np.zeros(12)
+    # # platepar.y_poly = np.zeros(12)
+    # #####
+
+    # print('Star X, Y:', star_x, star_y)
+
+    # jd, ra, dec, mag = XY2CorrectedRADecPP(np.array([time]), np.array([star_x]), np.array([star_y]), np.array([0]), platepar)
+
 
     # ra_h = int(ra/15)
     # ra_min = int((ra/15 - ra_h)*60)
@@ -786,34 +802,17 @@ if __name__ == "__main__":
     # dec_min = int((dec - dec_d)*60)
     # dec_sec = ((dec - dec_d)*60 - dec_min)*60
 
+    # print('Computed RA, Dec:')
+    # print(ra_h, ra_min, ra_sec[0])
+    # print(dec_d, dec_min, dec_sec[0])
 
-    # print(ra_h, ra_min, ra_sec)
-    # print(dec_d, dec_min, dec_sec)
 
+    # # Convert the coordinates of Sirius back to image coordinates
+    # # ra_star = (6 + (45 + 8/60)/60)*15
+    # # dec_star = -(16 + (43 + 21/60)/60)
+    # ra_star = ra[0]
+    # dec_star = dec[0]
+    # x_star, y_star = raDecToCorrectedXYPP(np.array([ra_star]), np.array([dec_star]), \
+    #     np.array([date2JD(*time)]), platepar)
 
-
-    # # Load the platepar
-    # platepar = Platepar()
-    # platepar.read("C:\\Users\\delorayn1\\Desktop\\2017071819-Processed\\platepar_cmn2010.cal")
-
-    # # X_scale = platepar.X_res/384
-    # # Y_scale = platepar.Y_res/288
-
-    # # x = (617.71 - platepar.X_res/2)/X_scale/platepar.F_scale
-    # # y = (139.99 - platepar.Y_res/2)/Y_scale/platepar.F_scale
-
-    # x = 617.71
-    # y = 139.99
-
-    # secs = 28.788 + 164.0/25
-    # time_data = [[2017, 7, 18, 20, 46, int(secs), int(1000*(secs - int(secs)))]]
-
-    # X_corrected, Y_corrected, levels_corrected = applyFieldCorrection(platepar.x_poly, platepar.y_poly, platepar.X_res, platepar.Y_res, platepar.F_scale, [x], [y], [0])
-
-    # az_data, alt_data = XY2altAz(platepar.lat, platepar.lon, platepar.RA_d, platepar.dec_d, platepar.Ho, platepar.pos_angle_ref, X_corrected, Y_corrected)
-
-    # # Convert azimuth and altitude data to right ascension and declination
-    # JD_data, RA_data, dec_data = altAz2RADec(platepar.lat, platepar.lon, 0, time_data, az_data, alt_data)
-
-    # print(az_data, alt_data)
-    # print(RA_data, dec_data)
+    # print('Star X, Y computed:', x_star, y_star)

RMS/Astrometry/AstrometryCheckFit.py
@@ -54,15 +54,14 @@ def matchStarsResiduals(config, platepar, catalog_stars, star_dict, match_radius
 
 
     # Estimate the FOV radius
-    x_scale = 384.0/platepar.X_res/platepar.F_scale
-    y_scale = 288.0/platepar.Y_res/platepar.F_scale
-    fov_w = x_scale*platepar.X_res
-    fov_h = y_scale*platepar.Y_res
+    fov_w = platepar.X_res/platepar.F_scale
+    fov_h = platepar.Y_res/platepar.F_scale
 
     fov_radius = np.sqrt((fov_w/2)**2 + (fov_h/2)**2)
 
     # print('fscale', platepar.F_scale)
-    # print('FOVw:', platepar.X_res/platepar.F_scale)
+    # print('FOV w:', fov_w)
+    # print('FOV h:', fov_h)
     # print('FOV radius:', fov_radius)
 
 
@@ -103,45 +102,12 @@ def matchStarsResiduals(config, platepar, catalog_stars, star_dict, match_radius
 
         # # Plot image stars
         # im_y, im_x, _, _ = stars_list.T
-        # plt.scatter(im_y, im_x, c='r', s=5)
+        # plt.scatter(im_y, im_x, facecolors='none', edgecolor='g')
 
         # # Plot catalog stars
-        # plt.scatter(cat_y_array[cat_good_indices], cat_x_array[cat_good_indices], facecolors='none', edgecolor='g')
+        # plt.scatter(cat_y_array[cat_good_indices], cat_x_array[cat_good_indices], c='r', s=20, marker='+')
 
         # plt.show()
-        
-        
-        #matched_indices = []
-
-        # # Match image and catalog stars
-        # # Go through all image stars
-        # for i, entry in enumerate(stars_list):
-
-        #     # Extract image star data
-        #     im_star_y, im_star_x, _, _ = entry
-
-        #     min_dist = np.inf
-        #     cat_match_indx = None
-
-        #     # Check for the best match among catalog stars
-        #     for k in cat_good_indices:
-
-        #         cat_x = cat_x_array[k]
-        #         cat_y = cat_y_array[k]
-
-
-        #         # Calculate the distance between stars
-        #         dist = np.sqrt((im_star_x - cat_x)**2 + (im_star_y - cat_y)**2)
-
-        #         if (dist < min_dist):
-        #             min_dist = dist
-        #             cat_match_indx = k
-
-
-        #     # Take the best matched star if the distance was within the maximum radius
-        #     if min_dist < match_radius:
-        #         matched_indices.append([i, cat_match_indx, min_dist])
-
 
 
         # Match image and catalog stars
@@ -471,7 +437,7 @@ def autoCheckFit(config, platepar, calstars_list):
     fatol = (config.dist_check_threshold**2)/np.sqrt(len(star_dict)*config.min_matched_stars + 1)
 
     # Parameter estimation tolerance for angular values
-    fov_w = 384.0/platepar.F_scale
+    fov_w = platepar.X_res/platepar.F_scale
     xatol_ang = config.dist_check_threshold*fov_w/platepar.X_res
 
 

RMS/Astrometry/SkyFit.py
@@ -449,6 +449,30 @@ class PlateTool(object):
             self.nextFF()
 
 
+        elif event.key == ',':
+
+            # Decrement UT correction
+            self.platepar.UT_corr -= 0.5
+
+            # Update platepar JD
+            self.platepar.JD += 0.5/24
+
+            print(self.platepar.JD)
+
+            self.updateImage()
+
+        elif event.key == '.':
+
+            # Decrement UT correction
+            self.platepar.UT_corr += 0.5
+
+            # Update platepar JD
+            self.platepar.JD -= 0.5/24
+
+            print(self.platepar.JD)
+
+            self.updateImage()            
+
         # Move RA/Dec
         elif event.key == 'a':
             self.platepar.RA_d -= self.key_increment
@@ -499,6 +523,32 @@ class PlateTool(object):
 
         elif event.key == 'down':
             self.platepar.F_scale *= 1.0 - self.key_increment/100.0
+            self.updateImage()
+
+
+        elif event.key == '1':
+
+            # Decrement X 1st order distortion
+            self.platepar.x_poly[1] -= 0.01
+            self.updateImage()
+
+        elif event.key == '2':
+
+            # Increment X 1st order distortion
+            self.platepar.x_poly[1] += 0.01
+            self.updateImage()
+
+
+        elif event.key == '3':
+
+            # Decrement Y 1st order distortion
+            self.platepar.y_poly[2] -= 0.01
+            self.updateImage()
+
+        elif event.key == '4':
+
+            # Increment Y 1st order distortion
+            self.platepar.y_poly[2] += 0.01
             self.updateImage()
 
 
@@ -585,16 +635,19 @@ class PlateTool(object):
         # Do a fit on the selected stars while in the star picking mode
         elif event.key == 'ctrl+z':
 
-            if self.star_pick_mode:
+            # if self.star_pick_mode:
 
-                # Do 3 fit iterations
-                for i in range(3):
+            #     # Do 3 fit iterations
+            #     for i in range(3):
 
-                    print('Fitting iteration {:d}/3'.format(i + 1))
+            #         print('Fitting iteration {:d}/3'.format(i + 1))
 
-                    self.fitPickedStars()
+            #         self.fitPickedStars()
 
-                print('Plate fitted!')
+            #     print('Plate fitted!')
+
+            self.fitPickedStars()
+            print('Plate fitted!')
 
 
         elif event.key == 'enter':
@@ -885,9 +938,9 @@ class PlateTool(object):
 
         ### Draw catalog stars on the image using the current platepar ###
         ######################################################################################################
-        self.catalog_x, self.catalog_y, catalog_mag = self.getCatalogStarPositions(self.catalog_stars, self.platepar.lon, 
-            self.platepar.lat, self.platepar.az_centre, self.platepar.alt_centre, self.platepar.pos_angle_ref, 
-            self.platepar.F_scale, self.platepar.x_poly, self.platepar.y_poly)
+        self.catalog_x, self.catalog_y, catalog_mag = self.getCatalogStarPositions(self.catalog_stars, \
+            self.platepar.lon, self.platepar.lat, self.platepar.RA_d, self.platepar.dec_d, 
+            self.platepar.pos_angle_ref, self.platepar.F_scale, self.platepar.x_poly, self.platepar.y_poly)
 
         if self.catalog_stars_visible:
             cat_stars = np.c_[self.catalog_x, self.catalog_y, catalog_mag]
@@ -927,6 +980,7 @@ class PlateTool(object):
         if self.show_key_help:
             # Show text on image with platepar parameters
             text_str  = self.current_ff_file + '\n\n'
+            text_str += 'UT corr = {:.1f}\n'.format(self.platepar.UT_corr)
             text_str += 'Ref RA  = {:.3f}\n'.format(self.platepar.RA_d)
             text_str += 'Ref Dec = {:.3f}\n'.format(self.platepar.dec_d)
             text_str += 'PA      = {:.3f}\n'.format(self.platepar.pos_angle_ref)
@@ -943,6 +997,9 @@ class PlateTool(object):
             text_str += 'Dec - S/W\n'
             text_str += 'PA  - Q/E\n'
             text_str += 'F_scale - Up/Down\n'
+            text_str += 'X 1st dist coeff - 1/2\n'
+            text_str += 'Y 1st dist coeff - 3/4\n'
+            text_str += 'UT correction - ,/.\n'
             text_str += 'Lim mag - R/F\n'
             text_str += 'Increment - +/-\n'
             text_str += 'Img Gamma - U/J\n'
@@ -1016,8 +1073,11 @@ class PlateTool(object):
         dec_centre = dec_centre[0]
 
         # Calculate the FOV radius in degrees
-        fov_x = (self.platepar.X_res/2)*(3600/self.platepar.F_scale)*(384/self.platepar.X_res)/3600
-        fov_y = (self.platepar.Y_res/2)*(3600/self.platepar.F_scale)*(288/self.platepar.Y_res)/3600
+        # fov_x = (self.platepar.X_res/2)*(3600/self.platepar.F_scale)*(384/self.platepar.X_res)/3600
+        # fov_y = (self.platepar.Y_res/2)*(3600/self.platepar.F_scale)*(288/self.platepar.Y_res)/3600
+
+        fov_x = (self.platepar.X_res/2)*(3600/self.platepar.F_scale)/3600
+        fov_y = (self.platepar.Y_res/2)*(3600/self.platepar.F_scale)/3600
 
         fov_radius = np.sqrt(fov_x**2 + fov_y**2)*2
 
@@ -1063,7 +1123,7 @@ class PlateTool(object):
 
 
 
-    def getCatalogStarPositions(self, catalog_stars, lon, lat, az_centre, alt_centre, pos_angle_ref, F_scale, 
+    def getCatalogStarPositions(self, catalog_stars, lon, lat, ra_ref, dec_ref, pos_angle_ref, F_scale, \
         x_poly, y_poly):
         """ Get image positions of catalog stars using the current platepar values. 
     
@@ -1071,8 +1131,8 @@ class PlateTool(object):
             catalog_stars: [2D list] A list of (ra, dec, mag) pairs of catalog stars.
             lon: [float] Longitude in degrees.
             lat: [float] Latitude in degrees.
-            az_centre: [float] Azimuth of the FOV centre at referent time in degrees.
-            alt_centre: [float] Altitude of the FOV centre at referent time in degrees.
+            ra_ref: [float] Referent RA of the FOV centre (degrees).
+            dec_ref: [float] Referent Dec of the FOV centre (degrees).
             pos_angle_ref: [float] Referent position angle in degrees.
             F_scale: [float] Image scale in pix/arcsec for CIF resolution.
             x_poly: [ndarray float] Distorsion polynomial in X direction.
@@ -1087,12 +1147,12 @@ class PlateTool(object):
         ff_middle_time = getMiddleTimeFF(self.current_ff_file, self.config.fps, ret_milliseconds=True)
 
         # Get the date of the middle of the FF exposure
-        jd = date2JD(*ff_middle_time, UT_corr=self.platepar.UT_corr)
+        jd = date2JD(*ff_middle_time)
 
         # Convert star RA, Dec to image coordinates
         x_array, y_array = raDecToCorrectedXY(ra_catalog, dec_catalog, jd, lat, lon, self.platepar.X_res, \
-            self.platepar.Y_res, self.platepar.RA_d, self.platepar.dec_d, self.platepar.JD, pos_angle_ref, \
-            F_scale, x_poly, y_poly)
+            self.platepar.Y_res, ra_ref, dec_ref, self.platepar.JD, pos_angle_ref, F_scale, x_poly, y_poly, 
+            UT_corr=self.platepar.UT_corr)
 
         return x_array, y_array, mag_catalog
 
@@ -1118,7 +1178,7 @@ class PlateTool(object):
 
         # Set the referent hour angle
         T = (self.platepar.JD - 2451545)/36525.0
-        Ho = 280.46061837 + 360.98564736629*(self.platepar.JD - 2451545) + 0.000387933*T**2 \
+        Ho = 280.46061837 + 360.98564736629*(self.platepar.JD - 2451545.0) + 0.000387933*T**2 \
             - (T**3)/38710000.0
 
         self.platepar.Ho = Ho
@@ -1130,8 +1190,8 @@ class PlateTool(object):
         print(time_data)
 
         # Convert FOV centre to RA, Dec
-        _, ra_data, dec_data = altAz2RADec(self.platepar.lat, self.platepar.lon, self.platepar.UT_corr, time_data, 
-            [self.azim_centre], [self.alt_centre])
+        _, ra_data, dec_data = altAz2RADec(self.platepar.lat, self.platepar.lon, self.platepar.UT_corr, 
+            time_data, [self.azim_centre], [self.alt_centre])
 
         return ra_data[0], dec_data[0]
 
@@ -1197,8 +1257,10 @@ class PlateTool(object):
         self.platepar.Y_res = self.config.height
 
         # Estimate the scale
-        scale_x = self.config.fov_w/self.config.width*(self.config.width/384)
-        scale_y = self.config.fov_h/self.config.height*(self.config.height/288)
+        # scale_x = self.config.fov_w/self.config.width*(self.config.width/384)
+        # scale_y = self.config.fov_h/self.config.height*(self.config.height/288)
+        scale_x = self.config.fov_w/self.config.width
+        scale_y = self.config.fov_h/self.config.height
         self.platepar.F_scale = 1/((scale_x + scale_y)/2)
 
         # Set distorsion polynomials to zero
@@ -1415,23 +1477,17 @@ class PlateTool(object):
             # Extract fitting parameters
             ra_ref, dec_ref, pos_angle_ref, F_scale = params
 
-            # Calculate the centre of FOV
-            az_centre, alt_centre = calcRefCentre(self.platepar.JD, self.platepar.lon, self.platepar.lat, 
-                ra_ref, dec_ref)
-
             img_x, img_y, _ = img_stars.T
 
             # Get image coordinates of catalog stars
             catalog_x, catalog_y, catalog_mag = self.getCatalogStarPositions(catalog_stars, self.platepar.lon, 
-                self.platepar.lat, az_centre, alt_centre, pos_angle_ref, F_scale, self.platepar.x_poly, 
+                self.platepar.lat, ra_ref, dec_ref, pos_angle_ref, F_scale, self.platepar.x_poly, 
                 self.platepar.y_poly)
 
 
-            dist_sum = 0
-
+            
             # Calculate the sum of squared distances between image stars and catalog stars
-            for i in range(len(catalog_x)):
-                dist_sum += (catalog_x[i] - img_x[i])**2 + (catalog_y[i] - img_y[i])**2
+            dist_sum = np.sum((catalog_x - img_x)**2 + (catalog_y - img_y)**2)
 
 
             return dist_sum
@@ -1451,28 +1507,29 @@ class PlateTool(object):
             if dimension == 'x':
                 x_poly = params
                 y_poly = np.zeros(12)
+                #y_poly = self.platepar.y_poly
 
             else:
                 x_poly = np.zeros(12)
+                #x_poly = self.platepar.x_poly
                 y_poly = params
 
-            # Calculate the centre of FOV
-            az_centre, alt_centre = calcRefCentre(self.platepar.JD, self.platepar.lon, self.platepar.lat, 
-                self.platepar.RA_d, self.platepar.dec_d)
 
             img_x, img_y, _ = img_stars.T
 
             # Get image coordinates of catalog stars
             catalog_x, catalog_y, catalog_mag = self.getCatalogStarPositions(catalog_stars, self.platepar.lon, 
-                self.platepar.lat, az_centre, alt_centre, self.platepar.pos_angle_ref, self.platepar.F_scale, 
-                x_poly, y_poly)
+                self.platepar.lat, self.platepar.RA_d, self.platepar.dec_d, self.platepar.pos_angle_ref, 
+                self.platepar.F_scale, x_poly, y_poly)
 
 
-            dist_sum = 0
+            # Calculate the sum of squared distances between image stars and catalog stars, per every
+            #   dimension
+            if dimension == 'x':
+                dist_sum = np.sum((catalog_x - img_x)**2)
 
-            # Calculate the sum of squared distances between image stars and catalog stars
-            for i in range(len(catalog_x)):
-                dist_sum += (catalog_x[i] - img_x[i])**2 + (catalog_y[i] - img_y[i])**2
+            else:
+                dist_sum = np.sum((catalog_y - img_y)**2)
 
 
             return dist_sum
@@ -1488,6 +1545,7 @@ class PlateTool(object):
 
         print('DIS_X', _calcImageResidualsDistorsion(self.platepar.x_poly, self, catalog_stars, img_stars, 'x'))
 
+        print('DIS_Y', _calcImageResidualsDistorsion(self.platepar.x_poly, self, catalog_stars, img_stars, 'y'))
 
 
         # Initial parameters for the astrometric fit
@@ -1503,8 +1561,8 @@ class PlateTool(object):
         self.platepar.RA_d, self.platepar.dec_d, self.platepar.pos_angle_ref, self.platepar.F_scale = res.x
 
         # Recalculate centre
-        calcRefCentre(self.platepar.JD, self.platepar.lon, self.platepar.lat, self.platepar.RA_d, 
-            self.platepar.dec_d)
+        self.platepar.az_centre, self.platepar.alt_centre = calcRefCentre(self.platepar.JD, self.platepar.lon, 
+            self.platepar.lat, self.platepar.RA_d, self.platepar.dec_d)
 
 
         # Fit distorsion parameters in X direction

RMS/Formats/Platepar.py
@@ -106,20 +106,24 @@ class Platepar(object):
         self.UT_corr = 0
 
         self.Ho = 0
-        self.X_res = self.Y_res = self.focal_length = 0
+        self.X_res = 0
+        self.Y_res = 0
+        self.focal_length = 0
 
         # FOV centre
         self.RA_d = self.RA_H = self.RA_M = self.RA_S = 0
         self.dec_d = self.dec_D = self.dec_M = self.dec_S = 0
         self.pos_angle_ref = 0
 
-        self.az_centre = self.alt_centre = 0
+        self.az_centre = 0
+        self.alt_centre = 0
 
         # FOV scale
         self.F_scale = 0
         self.w_pix = 0
 
-        self.mag_0 = self.mag_lev = 1.0
+        self.mag_0 = 1.0
+        self.mag_lev = 1.0
 
         # Distortion fit
         self.x_poly = np.zeros(shape=(12,), dtype=np.float64)