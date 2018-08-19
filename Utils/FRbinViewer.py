865b1f042abcca3f2afd3cc048caa6020b3f3f87
Denis Vida <denis.vida@gmail.com>
2018-08-19 17:09:50 +0200

    added saving video frames

--- Utils/FRbinViewer.py
+++ Utils/FRbinViewer.py
@@ -18,16 +18,18 @@
 
 from __future__ import print_function, absolute_import, division
 
-import cv2
-import numpy as np
 import os
 import sys
+import argparse
+
+import cv2
+import numpy as np
 
 import RMS.ConfigReader as cr
 from RMS.Formats import FFfile, FRbin
 
 
-def view(dir_path, ff_path, fr_path, config):
+def view(dir_path, ff_path, fr_path, config, save_frames=False):
     """ Shows the detected fireball stored in the FR file. 
     
     Arguments:
@@ -46,8 +48,8 @@ def view(dir_path, ff_path, fr_path, config):
         #background = np.zeros((config.height, config.width), np.uint8)
 
         # Get the maximum extent of the meteor frames
-        y_size = max(max(np.array(fr.yc[i]) + np.array(fr.size[i])//2) for i in range(fr.lines))
-        x_size = max(max(np.array(fr.xc[i]) + np.array(fr.size[i])//2) for i in range(fr.lines))
+        y_size = max(max(np.array(fr.yc[0]) + np.array(fr.size[0])//2) for i in range(fr.lines))
+        x_size = max(max(np.array(fr.xc[0]) + np.array(fr.size[0])//2) for i in range(fr.lines))
 
         # Make the image square
         img_size = max(y_size, x_size)
@@ -61,40 +63,48 @@ def view(dir_path, ff_path, fr_path, config):
     
     first_image = True
 
-    for i in range(fr.lines):
+    for current_line in range(fr.lines):
 
         print('Frame,  Y ,  X , size')
 
-        for z in range(fr.frameNum[i]):
+        for z in range(fr.frameNum[current_line]):
 
             # Get the center position of the detection on the current frame
-            yc = fr.yc[i][z]
-            xc = fr.xc[i][z]
+            yc = fr.yc[current_line][z]
+            xc = fr.xc[current_line][z]
 
             # Get the frame number
-            t = fr.t[i][z]
+            t = fr.t[current_line][z]
 
             # Get the size of the window
-            size = fr.size[i][z]
+            size = fr.size[current_line][z]
             
             print("  {:3d}, {:3d}, {:3d}, {:d}".format(t, yc, xc, size))
+
+            img = np.copy(background)
             
-            
-            y2 = 0
+            # Paste the frames onto the big image
+            y_img = np.arange(yc - size//2, yc + size//2)
+            x_img = np.arange(xc - size//2,  xc + size//2)
 
-            # Assign the detection pixels to the background image
-            for y in range(yc - size//2, yc + size//2):
+            Y_img, X_img = np.meshgrid(y_img, x_img)
 
-                x2 = 0
+            y_frame = np.arange(len(y_img))
+            x_frame = np.arange(len(x_img))
 
-                for x in range(xc - size//2,  xc + size//2):
+            Y_frame, X_frame = np.meshgrid(y_frame, x_frame)                
 
-                    background[y, x] = fr.frames[i][z][y2, x2]
-                    x2 += 1
+            img[Y_img, X_img] = fr.frames[current_line][z][Y_frame, X_frame]
 
-                y2 += 1
-            
-            cv2.imshow(name, background)
+
+            # Save frame to disk
+            if save_frames:
+                frame_file_name = fr_path.replace('.bin') + "_frame_{:03d}.png".format(t)
+                cv2.imwrite(os.path.join(dir_path, frame_file_name), img)
+
+
+            # Show the frame
+            cv2.imshow(name, img)
 
             # If this is the first image, move it to the upper left corner
             if first_image:
@@ -108,11 +118,23 @@ def view(dir_path, ff_path, fr_path, config):
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 2:
-        print('Usage: python -m Utils.FRbinViewer /path/to/FRbin/dir/')
-        sys.exit()
+    ### COMMAND LINE ARGUMENTS
 
-    dir_path = sys.argv[1].replace('"', '')
+    # Init the command line arguments parser
+    arg_parser = argparse.ArgumentParser(description="Show reconstructed fireball detections from FR files.")
+
+    arg_parser.add_argument('dir_path', nargs=1, metavar='DIR_PATH', type=str, \
+        help='Path to the directory which contains FR bin files.')
+
+    arg_parser.add_argument('-e', '--extract', metavar='EXTRACT_FR_FILE', type=str, \
+        help="Save frames from a given FR file to disk.")
+
+    # Parse the command line arguments
+    cml_args = arg_parser.parse_args()
+
+    #########################
+
+    dir_path = cml_args.dir_path[0]
 
     # Load the configuration file
     config = cr.parse(".config")
