fixed threshold computation

RMS/Detection.py
@@ -455,8 +455,8 @@ def getLines(img_handle, k1, j1, time_slide, time_window_size, max_lines, max_wh
 
 
 
-        # Show thresholded image
-        show(str(frame_min) + "-" + str(frame_max) + " threshold", img)
+        # # Show thresholded image
+        # show(str(frame_min) + "-" + str(frame_max) + " threshold", img)
 
         # # Show maxpixel of the thresholded part
         # mask = np.zeros(shape=img.shape)
@@ -927,8 +927,8 @@ def detectMeteors(img_handle, config, flat_struct=None):
         logDebug('Number of KHT lines: ', len(line_list))
         logDebug(line_list)
 
-        # Plot lines
-        plotLines(img_handle.ff, line_list)
+        # # Plot lines
+        # plotLines(img_handle.ff, line_list)
 
 
         filtered_lines = []
@@ -1008,8 +1008,8 @@ def detectMeteors(img_handle, config, flat_struct=None):
 
                 # logDebug(detected_line)
 
-                # Show 3D cloud
-                show3DCloud(img_handle.ff, xs, ys, zs, detected_line, stripe_points, config)
+                # # Show 3D cloud
+                # show3DCloud(img_handle.ff, xs, ys, zs, detected_line, stripe_points, config)
 
                 # Add the line to the results list
                 filtered_lines.append(detected_line)
@@ -1075,12 +1075,6 @@ def detectMeteors(img_handle, config, flat_struct=None):
 
                 img_thres, max_avg_corrected, flattened_weights, \
                     min_patch_intensity = thresholdAndCorrectGammaFF(img_handle, config)
-
-
-                
-                plt.imshow(img_thres, cmap='gray')
-                plt.title('centroiding')
-                plt.show()
                 
 
 
@@ -1140,12 +1134,12 @@ def detectMeteors(img_handle, config, flat_struct=None):
                     if config.deinterlace_order >= 0:
 
                         # Deinterlace by fields (line lixels)
-                        half_frame_pixels = frame_pixels[frame_pixels[:,1] % 2 == (config.deinterlace_order 
-                            + half_frame) % 2]
+                        half_frame_pixels = frame_pixels[frame_pixels[:,1]%2 == (config.deinterlace_order 
+                            + half_frame)%2]
 
                         # Deinterlace by fields (stripe pixels)
                         half_frame_pixels_stripe = frame_pixels_stripe[frame_pixels_stripe[:,1] % 2 == (config.deinterlace_order 
-                            + half_frame) % 2]
+                            + half_frame)%2]
 
 
                         # Skip if there are no pixels in the half-frame
@@ -1153,7 +1147,7 @@ def detectMeteors(img_handle, config, flat_struct=None):
                             continue
 
                         # Calculate half-frame value
-                        frame_no = i+half_frame*0.5
+                        frame_no = i + half_frame*0.5
 
 
                     # No deinterlacing

RMS/DetectionTools.py
@@ -185,20 +185,19 @@ def getThresholdedStripe3DPoints(config, img_handle, frame_min, frame_max, rho, 
                 fr_img = Image.applyFlat(fr_img, flat_struct)
                 
 
-
-            # plt.imshow(fr_img, cmap='gray', vmin=100, vmax=10000)
-            # plt.show()
-
             # Threshold the frame
             img_thres = Image.thresholdImg(fr_img, img_handle.ff.avepixel, img_handle.ff.stdpixel, \
                 config.k1_det, config.j1_det)
 
 
-            print(fr)
-            fig, (ax1, ax2) = plt.subplots(nrows=2)
-            ax1.imshow(img_thres, cmap='gray')
-            ax2.imshow(fr_img, cmap='gray', vmax=10000)
-            plt.show()
+            # print(fr)
+            # print('mean stdpixel3:', np.mean(img_handle.ff.stdpixel))
+            # print('mean avepixel3:', np.mean(img_handle.ff.avepixel))
+            # print('mean frame:', np.mean(fr_img))
+            # fig, (ax1, ax2) = plt.subplots(nrows=2)
+            # ax1.imshow(img_thres, cmap='gray')
+            # ax2.imshow(img_handle.ff.maxpixel - fr_img, cmap='gray', vmax=10000)
+            # plt.show()
 
             # Remove lonely pixels
             img_thres = morph.clean(img_thres)

RMS/Routines/Image.py
@@ -13,11 +13,11 @@ from RMS.Decorators import memoizeSingle
 
 
 
-def thresholdImg(maxpixel, avepixel, stdpixel, k1, j1):
+def thresholdImg(img, avepixel, stdpixel, k1, j1):
     """ Threshold the image with given parameters.
     
     Arguments:
-        maxpixel: [2D ndarray]
+        img: [2D ndarray]
         avepixel: [2D ndarray]
         stdpixel: [2D ndarray]
         k1: [float] relative thresholding factor (how many standard deviations above mean the maxpixel image 
@@ -29,8 +29,11 @@ def thresholdImg(maxpixel, avepixel, stdpixel, k1, j1):
         [ndarray] thresholded 2D image
     """
 
+    # Subtract input image and average, making sure there are no values below 0 which will wrap around
+    img_avg_sub = applyDark(img, avepixel)
+
     # Compute the thresholded image
-    img_thresh = maxpixel - avepixel > (k1 * stdpixel + j1)
+    img_thresh = img_avg_sub > (k1 * stdpixel + j1)
 
     # The thresholded image is always 8 bit
     return img_thresh.astype(np.uint8)