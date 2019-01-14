5d90c8933f9a28e59f8da1c746685c5b2335cf89
denis.vida <denis.vida@gmail.com>
2019-01-14 17:56:00 -0500

    added more advanced stacking

--- Utils/StackFFs.py
+++ Utils/StackFFs.py
@@ -11,48 +11,11 @@ import scipy.misc
 
 from RMS.Formats.FFfile import read as readFF
 from RMS.Formats.FFfile import validFFName
-from RMS.Routines.Image import deinterlaceBlend, blendLighten, loadFlat, applyFlat
+from RMS.Routines.Image import deinterlaceBlend, blendLighten, loadFlat, applyFlat, adjustLevels
 
 
 
-
-if __name__ == '__main__':
-
-    ### COMMAND LINE ARGUMENTS
-
-    # Init the command line arguments parser
-    arg_parser = argparse.ArgumentParser(description="Stacks all maxpixles in the given folder to one image.")
-
-    arg_parser.add_argument('dir_path', nargs=1, metavar='DIR_PATH', type=str, \
-        help='Path to directory with FF files.')
-
-    arg_parser.add_argument('file_format', nargs=1, metavar='FILE_FORMAT', type=str, \
-        help='File format of the image, e.g. jpg or png.')
-
-    arg_parser.add_argument('-d', '--deinterlace', action="store_true", \
-        help="""Deinterlace the image before stacking. """)
-
-    arg_parser.add_argument('-s', '--subavg', action="store_true", \
-        help="""Subtract the average image from maxpixel before stacking. """)
-
-    arg_parser.add_argument('-f', '--flat', nargs='?', metavar='FLAT_PATH', type=str, default='', 
-        help="Apply a given flat frame. If no path to the flat is given, flat.bmp from the folder will be taken.")
-
-    # Parse the command line arguments
-    cml_args = arg_parser.parse_args()
-
-    #########################
-
-
-    dir_path = cml_args.dir_path[0]
-
-    first_img = True
-
-
-    # Check if a flat was given
-    flat_path = cml_args.flat
-
-    print(flat_path)
+def stackFFs(dir_path, file_format, deinterlace=False, subavg=False, filter_bright=False, flat_path=None):
 
     # Load the flat if it was given
     flat = None
@@ -75,11 +38,14 @@ if __name__ == '__main__':
             print('Loaded flat:', flat_full_path)
 
 
-    # List all FF files in the current dir
-    for ff_name in os.listdir(dir_path):
-        if validFFName(ff_name):
+    first_img = True
 
-            print('Stacking: ', ff_name)
+    n_stacked = 0
+    total_ff_files = 0
+
+    # List all FF files in the current dir
+    for ff_name in sorted(os.listdir(dir_path)):
+        if validFFName(ff_name):
 
             # Load FF file
             ff = readFF(dir_path, ff_name)
@@ -88,42 +54,118 @@ if __name__ == '__main__':
             if ff is None:
                 continue
 
+            total_ff_files += 1
+
             maxpixel = ff.maxpixel
             avepixel = ff.avepixel
 
             # Dinterlace the images
-            if cml_args.deinterlace:
+            if deinterlace:
                 maxpixel = deinterlaceBlend(maxpixel)
                 avepixel = deinterlaceBlend(avepixel)
 
             # If the flat was given, apply it to the image, only if no subtraction is done
-            if (flat is not None) and not cml_args.subavg:
+            if (flat is not None) and not subavg:
                 maxpixel = applyFlat(maxpixel, flat)
                 avepixel = applyFlat(avepixel, flat)
 
+
+            # Reject the image if the median subtracted image is too bright. This usually means that there
+            #   are clouds on the image which can ruin the stack
+            if filter_bright:
+
+                img = maxpixel - avepixel
+
+                # Compute surface brightness
+                median = np.median(img)
+
+                # Compute top detection pixels
+                top_brightness = np.percentile(img, 99.9)
+
+                # Reject all images where the median brightness is high
+                # Preserve images with very bright detections
+                if (median > 10) and (top_brightness < (2**(8*img.itemsize) - 10)):
+                    print('Skipping: ', ff_name, 'median:', median, 'top brightness:', top_brightness)
+                    continue
+
+
             # Subtract the average from maxpixel
-            if cml_args.subavg:
+            if subavg:
                 img = maxpixel - avepixel
 
             else:
                 img = maxpixel
-
 
             if first_img:
                 merge_img = np.copy(img)
                 first_img = False
                 continue
 
+            print('Stacking: ', ff_name)
+
             # Blend images 'if lighter'
             merge_img = blendLighten(merge_img, img)
 
+            n_stacked += 1
 
-    stack_path = os.path.join(dir_path, 'stacked.' + cml_args.file_format[0])
 
-    print("Saving to:", stack_path)
+    # If the number of stacked image is less than 20% of the given images, stack without filtering
+    if n_stacked < 0.2*total_ff_files:
+        return stackFFs(dir_path, file_format, deinterlace=deinterlace, subavg=subavg, 
+            filter_bright=False, flat_path=flat_path)
+
+
+
+    stack_path = os.path.join(dir_path, 'stacked.' + file_format)
+
+    print("Saving stack to:", stack_path)
+
+    # Stretch the levels
+    merge_img = adjustLevels(merge_img, np.percentile(merge_img, 0.5), 1.3, np.percentile(merge_img, 99.9))
+
     
     # Save the blended image
     scipy.misc.imsave(stack_path, merge_img)
+
+
+    return stack_path, merge_img
+
+
+
+if __name__ == '__main__':
+
+    ### COMMAND LINE ARGUMENTS
+
+    # Init the command line arguments parser
+    arg_parser = argparse.ArgumentParser(description="Stacks all maxpixles in the given folder to one image.")
+
+    arg_parser.add_argument('dir_path', nargs=1, metavar='DIR_PATH', type=str, \
+        help='Path to directory with FF files.')
+
+    arg_parser.add_argument('file_format', nargs=1, metavar='FILE_FORMAT', type=str, \
+        help='File format of the image, e.g. jpg or png.')
+
+    arg_parser.add_argument('-d', '--deinterlace', action="store_true", \
+        help="""Deinterlace the image before stacking. """)
+
+    arg_parser.add_argument('-s', '--subavg', action="store_true", \
+        help="""Subtract the average image from maxpixel before stacking. """)
+
+    arg_parser.add_argument('-b', '--brightfilt', action="store_true", \
+        help="""Rejects images with very bright background, which are often clouds. """)
+
+    arg_parser.add_argument('-f', '--flat', nargs='?', metavar='FLAT_PATH', type=str, default='', 
+        help="Apply a given flat frame. If no path to the flat is given, flat.bmp from the folder will be taken.")
+
+    # Parse the command line arguments
+    cml_args = arg_parser.parse_args()
+
+    #########################
+
+    # Run stacking
+    stack_path, merge_img = stackFFs(cml_args.dir_path[0], cml_args.file_format[0], \
+        deinterlace=cml_args.deinterlace, subavg=cml_args.subavg, filter_bright=cml_args.brightfilt, \
+        flat_path=cml_args.flat)
 
     # Plot the blended image
     plt.imshow(merge_img, cmap='gray', vmin=0, vmax=255)
