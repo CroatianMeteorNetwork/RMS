69dba9d9a0f63317af9a0550f43058c97e9064ec
Denis Vida <denis.vida@gmail.com>
2019-02-07 13:00:34 -0500

    added script for batch recomputing magnitudes with a given photometric offset

--- RMS/Formats/AsgardEv.py
+++ RMS/Formats/AsgardEv.py
@@ -4,10 +4,205 @@ from __future__ import print_function, division, absolute_import
 
 
 import os
+import datetime
 
 import numpy as np
 
 from RMS.Astrometry.Conversions import jd2UnixTime, jd2Date
+
+
+class AsgardEv(object):
+    def __init__(self):
+
+        self.version = ''
+        self.num_fr = 0
+        self.num_tr = 0
+        self.time = ''
+        self.unix = 0.0
+        self.ntp = ''
+        self.seq = 0
+        self.mul = ''
+        self.site = ''
+        self.latlon = []
+        self.text = ''
+        self.stream = ''
+        self.plate = ''
+        self.geom = []
+        self.filter = 0
+
+        self.data = None
+
+
+    def __repr__(self):
+        """ Returns a string formatted as the ASGARD ev file. """
+
+        out_list = []
+
+        out_list.append(['version', self.version])
+        out_list.append(['num_fr', str(self.num_fr)])
+        out_list.append(['num_tr', str(self.num_tr)])
+        out_list.append(['time', self.time.strftime("%Y%m%d %H:%M:%S.%f")[:-3] + " UTC"])
+        out_list.append(['unix', "{:.6f}".format(self.unix)])
+        out_list.append(['ntp', self.ntp])
+        out_list.append(['seq', str(self.seq)])
+        out_list.append(['mul', self.mul])
+        out_list.append(['site', str(self.site)])
+        out_list.append(['latlon', "{:.6f} {:.6f} {:.1f}".format(*self.latlon)])
+        out_list.append(['text', self.text])
+        out_list.append(['stream', self.stream])
+        out_list.append(['plate', self.plate])
+        out_list.append(['geom', "{:d} {:d}".format(*self.geom)])
+        out_list.append(['filter', str(self.filter)])
+
+        out_str =  "#\n"
+
+        for key, value in out_list:
+            out_str += "# {:>9s} : {:s}\n".format(key, value)
+
+        out_str += "#\n"
+        out_str += "#  fr    time    sum     seq       cx       cy      th      phi     lsp    mag  flag   bak    max\n"
+        
+        fr_arr, time_arr, sum_arr, seq_arr, cx_arr, cy_arr, th_arr, phi_arr, lsp_arr, mag_arr, flag_arr, \
+            bak_arr, max_arr = self.data.T
+
+        fr_arr = fr_arr.astype(np.int)
+        sum_arr = sum_arr.astype(np.int)
+        seq_arr = seq_arr.astype(np.int)
+        flag_arr = flag_arr.astype(np.int)
+
+        for i in range(len(self.data)):
+            out_str += "{:5d} {:7.3f} {:6d} {:7d} {:8.3f} {:8.3f} {:7.3f} {:8.3f} {:7.3f} {:6.2f}  {:04d} {:5.1f} {:6.1f}\n".format(\
+                fr_arr[i], time_arr[i], sum_arr[i], seq_arr[i], cx_arr[i], cy_arr[i], th_arr[i], \
+                phi_arr[i], lsp_arr[i], mag_arr[i], flag_arr[i], bak_arr[i], max_arr[i])
+
+
+        return out_str
+
+
+    def write(self, file_path):
+        """ Write the ASGARD ev object to ev file. 
+        
+        Arguments:
+            file_path: [str] Path to the file which will be saved.
+
+        """
+
+        with open(file_path, 'w') as f:
+
+            for line in self.__repr__():
+                f.write(line)
+
+
+    def read(self, file_path):
+        """ Read an ASGARD ev file. 
+    
+        Arguments:
+            file_path: [str] Path to the ASGARD ev file.
+        """
+
+        readEv(*os.path.split(file_path), ev=self)
+
+        
+
+
+
+def readEv(dir_path, file_name, ev=None):
+    """ Read the UWO ASGARD style event file. 
+    
+    Arguments:
+        dir_path: [str] Directory where the file is.
+        file_name: [str] Name of the event file.
+
+    Keyword arguments:
+        ev: [AsgardEv instance] AsgardEv object to use. None by default, in which case a new object will be
+            inited.
+
+    Return:
+        ev: [AsgardEv instance] Instance of the AsgardEv object filled with data read from the file.
+
+    """
+
+    with open(os.path.join(dir_path, file_name)) as f:
+
+        if ev is None:
+            
+            # Init the new ev structure
+            ev = AsgardEv()
+
+
+        data_list = []
+
+        for line in f:
+
+            line = line.replace('\n', '').replace('\r', '')
+
+            # Read the header
+            if line.startswith('#'):
+
+                line = line[1:]
+
+                line = line.split(':', 1)
+
+                if len(line) != 2:
+                    continue
+
+                key, value = line
+                key = key.strip()
+                value = value.strip()
+
+                if key == 'version':
+                    ev.version = value
+
+                elif key == 'num_fr':
+                    ev.num_fr = int(value)
+
+                elif key == 'num_tr':
+                    ev.num_tr = int(value)
+
+                elif key == 'time':
+                    ev.time = datetime.datetime.strptime(value, "%Y%m%d %H:%M:%S.%f UTC")
+
+                elif key == 'unix':
+                    ev.unix = float(value)
+
+                elif key == 'ntp':
+                    ev.ntp = value
+
+                elif key == 'seq':
+                    ev.seq = int(value)
+
+                elif key == 'mul':
+                    ev.mul = value
+
+                elif key == 'site':
+                    ev.site = value
+
+                elif key == 'latlon':
+                    ev.latlon = list(map(float, value.split()))
+
+                elif key == 'text':
+                    ev.text = value
+
+                elif key == 'stream':
+                    ev.stream = value
+
+                elif key == 'plate':
+                    ev.plate = value
+
+                elif key == 'geom':
+                    ev.geom = list(map(int, value.split()))
+
+                elif key == 'filter':
+                    ev.filter = value
+
+            else:
+                data_list.append(line.split())
+
+
+        ev.data = np.array(data_list).astype(np.float64)
+
+        return ev
+
 
 
 def writeEv(dir_path, file_name, ev_array, platepar):
@@ -76,3 +271,45 @@ def writeEv(dir_path, file_name, ev_array, platepar):
 
             f.write("{:5d} {:7.3f} {:10d} {:7d} {:8.3f} {:8.3f} {:7.3f} {:8.3f} {:7.3f} {:6.2f}  0000   0.0    0.0\n".format(int(31 + int(seq_num) - seq_array[0]), \
                 t_rel, int(intensity), int(seq_num), x, y, theta, phi, -2.5*np.log10(intensity), mag))
+
+
+
+def batchRecomputeMagnitudes(dir_path, photom_offset):
+    """ Recompute the magnitudes in all ev files in the given directory. The files will be read in, the 
+        magnitudes recomputed, and the files will be saved back.
+
+    Arguments:
+        dir_path: [str] Path to the directory with ev files.
+        photom_offset: [float] The new photometric offset.
+
+    """
+
+    for file_name in os.listdir(dir_path):
+
+        if file_name.startswith('ev_') and file_name.endswith('.txt'):
+
+            print('Fixing magnitudes:', file_name)
+
+            # Read the ev file
+            ev = readEv(dir_path, file_name)
+
+            # Recompute the magnitudes
+            lsp_arr = ev.data[:, 8]
+            mag_arr = lsp_arr + photom_offset
+
+            # Save the new magnitudes
+            ev.data[:, 9] = mag_arr
+
+            # Save the ev file
+            ev.write(os.path.join(dir_path, file_name))
+
+
+
+
+if __name__ == "__main__":
+
+    dir_path = '/home/dvida/Desktop'
+    #file_name = "ev_20181014_051904A_02G.txt"
+
+    print('Recomputing magnitudes...')
+    batchRecomputeMagnitudes(dir_path, 16.0)
