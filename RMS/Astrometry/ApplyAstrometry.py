d02fad53b26ccd16d37e9c2fbdf61472704e6fae
Denis Vida <denis.vida@gmail.com>
2019-04-05 12:14:03 -0400

    fixed circular dependency

--- RMS/Astrometry/ApplyAstrometry.py
+++ RMS/Astrometry/ApplyAstrometry.py
@@ -42,7 +42,7 @@ import RMS.Formats.Platepar
 from RMS.Formats.FTPdetectinfo import readFTPdetectinfo, writeFTPdetectinfo
 from RMS.Formats.FFfile import filenameToDatetime
 from RMS.Math import angularSeparation
-from Utils.RMS2UFO import FTPdetectinfo2UFOOrbitInput
+from Utils import RMS2UFO
 
 # Import Cython functions
 import pyximport
@@ -1106,7 +1106,7 @@ if __name__ == "__main__":
 
 
     # Recompute the UFOOrbit file
-    FTPdetectinfo2UFOOrbitInput(dir_path, ftp_detectinfo_file, os.path.join(dir_path, platepar_file))
+    RMS2UFO.FTPdetectinfo2UFOOrbitInput(dir_path, ftp_detectinfo_file, os.path.join(dir_path, platepar_file))
 
     print('Done!')
 
