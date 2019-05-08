51f7f10392548babc6845fa86f3d6ad6f6c0fe2d
denis.vida <denis.vida@gmail.com>
2019-05-08 11:48:37 -0400

    fixed install different onvif libraries in py 2 and 3

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, Extension
 import os
+import sys
 
 kht_module = Extension("kht_module",
                     sources = ["Native/Hough/kht.cpp",
@@ -19,10 +20,40 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 
+
+
+### HANDLE DIFFERENT ONVIF LIBRARIES FOR Py 2 AND 3 ###
+
+# Python 2 uses the 'onvif' library and Python 3 uses the onvif_zeep library
+if sys.version_info[0] < 3:
+
+    onvif_str = 'onvif_zeep'
+    onvif_proper = 'onvif'
+
+else:
+    onvif_str = 'onvif'
+    onvif_proper = 'onvif_zeep'
+
+
+# Strip version numbers from requirements
+reqs_stripped = [req.split('==')[0] for req in requirements]
+reqs_stripped = [req.split('>=')[0] for req in reqs_stripped]
+reqs_stripped = [req.split('<=')[0] for req in reqs_stripped]
+
+if onvif_str in reqs_stripped:
+    onvif_index = reqs_stripped.index(onvif_str)
+
+# Replace the onvif module with the correct module for this version
+requirements[onvif_index] = onvif_proper
+
+### ###
+
+
+
 setup (name = "RMS",
-       version = "0.0",
-       description = "Video meteor station for under 100 bucks",
-       setup_requires=["numpy"],
-       install_requires=requirements,
-       ext_modules = [kht_module]
-       )
+        version = "0.1",
+        description = "Raspberry Pi Meteor Station",
+        setup_requires=["numpy"],
+        install_requires=requirements,
+        ext_modules = [kht_module]
+        )
