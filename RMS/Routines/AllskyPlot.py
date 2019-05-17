fd584761bcbb0196cc59feb2f8519a166cf3162b
denis.vida <denis.vida@gmail.com>
2019-05-17 16:27:29 -0400

    shifted ra by 180 deg to follow convention

--- RMS/Routines/AllskyPlot.py
+++ RMS/Routines/AllskyPlot.py
@@ -33,7 +33,7 @@ class AllSkyPlot(object):
 	def raDec2XY(self, ra, dec):
 
 		# Compute projected coordinates
-		x = (ra - self.ra0)*np.cos(np.radians(dec))
+		x = ((180 - ra)%360 - self.ra0)*np.cos(np.radians(dec))
 		y = dec
 
 		return x, y
@@ -44,7 +44,7 @@ class AllSkyPlot(object):
 		# If there are more than one point, check for 0/360 wraparounds in RA
 		if isinstance(ra_array, list) or isinstance(ra_array, np.ndarray):
 
-			ra_array = np.array(ra_array)%360
+			ra_array = np.array(180 - ra_array)%360
 			dec_array = np.array(dec_array)
 
 			coord_list = []
@@ -69,13 +69,23 @@ class AllSkyPlot(object):
 				coord_list.append([ra_array[break_idx + 1:], dec_array[break_idx + 1:]])
 
 		else:
-			coord_list = [[ra_array, dec_array]]
+			coord_list = [[180 - ra_array, dec_array]]
 
 
 		# Plot all segments
-		for ra_temp, dec_temp in coord_list:
-			x, y = self.raDec2XY(ra_temp, dec_temp)
-			plt.plot(x, y, **kwargs)
+		for i, (ra_temp, dec_temp) in enumerate(coord_list):
+			x, y = self.raDec2XY(180 - ra_temp, dec_temp)
+
+			# Make sure that all plotted lines have the same color
+			if i > 0:
+				color = plt_handle[0].get_color()
+
+				# Add color to kwargs
+				if 'color' not in kwargs:
+					kwargs['color'] = color
+			
+
+			plt_handle = plt.plot(x, y, **kwargs)
 
 
 
@@ -91,12 +101,12 @@ class AllSkyPlot(object):
 
 
 		# Plot a meridian and parallel grid
-		ra_grid = np.arange(0, 360 + step, step)
+		ra_grid = np.sort(np.append(np.arange(0, 360 + step, step), [180.0001]))
 		dec_grid = np.arange(-90, 90 + step, step)
 
 
 		# Plot meridians
-		for ra in ra_grid:
+		for ra in ra_grid[:-1]:
 
 			# Increase number of points for meridian plot so they are smoother
 			step_finer = step/5
@@ -114,15 +124,13 @@ class AllSkyPlot(object):
 
 			dec_temp = np.zeros_like(ra_grid) + dec
 
-			x_grid, y_grid = self.raDec2XY(ra_grid, dec_temp)
-
-			self.ax.plot(x_grid, y_grid, linestyle='dotted', alpha=0.5, color='gray')
+			self.plot(ra_grid, dec_temp, linestyle='dotted', alpha=0.5, color='gray')
 
 
-		# Plot ticks
-		for dec in dec_grid:
+		# Plot dec ticks
+		for dec in dec_grid[::2]:
 
-			x, y = self.raDec2XY(self.ra0 + 180, dec)
+			x, y = self.raDec2XY(0, dec)
 
 			if dec > 0:
 				va = 'bottom'
@@ -130,13 +138,18 @@ class AllSkyPlot(object):
 			else:
 				va = 'top'
 
-			self.ax.text(x, y, "{:+d}$^\circ$".format(dec), color='0.5', ha='left', va=va, size=7)
+			self.ax.text(x, y, "{:+d}$^\circ$".format(dec), color='0.5', ha='center', va=va, size=7)
+
 
 		# Plot every other RA tick and skip 0 and 360
-		for ra in ra_grid[0:-1:2]:
+		ra_ticks = np.sort(np.append(np.arange(0, 360, 2*step), [180.0001]))
+		for ra in ra_ticks:
+
+			# Offset RA so 0 starts in the middle and increases to the left
+			#ra_text = (180 - ra)%360
 
 			x, y = self.raDec2XY(ra, 0)
-			self.ax.text(x, y, "{:+d}$^\circ$".format(ra), color='0.5', ha='center', va='top', size=7)
+			self.ax.text(x, y, "{:+d}$^\circ$".format(int(ra)), color='0.5', ha='center', va='top', size=7)
 
 
 
