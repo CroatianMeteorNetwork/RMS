8d16dea3ca466ddea4a5f0b00fe429fdce175e39
denis.vida <denis.vida@gmail.com>
2019-07-31 10:16:32 -0400

    fixed generating avconv command

--- Utils/GenerateTimelapse.py
+++ Utils/GenerateTimelapse.py
@@ -92,7 +92,11 @@ if __name__ == "__main__":
 
 
     # Construct the ecommand for avconv            
-    com = "cd " + dir_path + ";avconv -v quiet -r 30 -y -i temp_%04d.jpg -flags:0 gray -vcodec libx264 -vsync passthrough -pix_fmt yuv420p -crf 25 -r 30 -vf lutyuv=\"y=gammaval=(1.3)\"" + dir_path + os.path.basename(os.path.dirname(dir_path)) + ".mp4"
+    mp4_path = os.path.join(dir_path, os.path.basename(os.path.dirname(dir_path)))
+    com = "cd " + dir_path + ";" \
+        + "avconv -v quiet -r 30 -y -i temp_%04d.jpg -flags:0 gray -vcodec libx264 -vsync passthrough -pix_fmt yuv420p -crf 25 -r 30 -vf lutyuv=\"y=gammaval=(1.3)\" " \
+        + mp4_path + ".mp4"
+
     print ("Creating timelapse by avconv...")
     print(com)
     subprocess.call([com], shell=True)
