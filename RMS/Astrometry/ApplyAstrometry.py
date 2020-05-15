80ff6a9ec6947b69fe193daf8ecb30cb32fef282
Denis Vida <denis.vida@gmail.com>
2020-05-15 09:55:10 -0400

    added forcing unicode literals

--- RMS/Astrometry/ApplyAstrometry.py
+++ RMS/Astrometry/ApplyAstrometry.py
@@ -23,7 +23,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from __future__ import print_function, division, absolute_import
+from __future__ import print_function, division, absolute_import, unicode_literals
 
 import os
 import sys
