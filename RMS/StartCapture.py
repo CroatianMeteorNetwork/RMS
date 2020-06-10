c0046482acb43ddbc83e8359cb55d8884d48c08d
markmac99 <gmn@gmn.uwo.ca>
2020-06-10 18:13:45 +0100

    testing adding a comment

--- RMS/StartCapture.py
+++ RMS/StartCapture.py
@@ -844,7 +844,7 @@ if __name__ == "__main__":
             time_now = datetime.datetime.utcnow()
             waiting_time = start_time - time_now
 
-            log.info('Waiting {:s} to start recording for {:.2f} hrs'.format(str(waiting_time), \
+            log.info('MM Waiting {:s} to start recording for {:.2f} hrs'.format(str(waiting_time), \
                 duration/60/60))
 
             # Reset the Ctrl+C to KeyboardInterrupt
