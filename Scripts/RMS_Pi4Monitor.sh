b1dd59ce54958fdb79550fc0fd3967af1f03fe93
markmac99 <gmn@gmn.uwo.ca>
2020-06-11 23:31:17 +0100

    fixed some bugs

--- Scripts/RMS_Pi4Monitor.sh
+++ Scripts/RMS_Pi4Monitor.sh
@@ -6,13 +6,13 @@ hn=`hostname`
 MAILRECIP=markmcintyre99@googlemail.com
 
 cd /home/pi/RMS_data/logs
-fn=`ls -1 log* | tail -1`
 dead="no"
 while [ "$dead" != "yes" ]
 do
+  fn=`ls -1tr log* | tail -1`
   logf=`find . -name $fn -mmin +1 -ls`
   dayt=`wc -l $fn | awk '{print $1}'`
-  if [[ "$logf" !=  "" && $dayt -gt 10 ]] ; then
+  if [[ "$logf" !=  "" && $dayt -gt 30 ]] ; then
     done=`grep Archiving $fn`
     if [ "$done" != "" ] ; then
         logger 'RMS Pi4 watchdog process finished cleanly'
@@ -24,7 +24,7 @@ do
     sleep 2
     cd ~/source/RMS
     lxterminal -e Scripts/RMS_StartCapture.sh -r
-
+    cd /home/pi/RMS_data/logs
     echo From: pi@${hn} > ./stopped.txt
     echo To: $MAILRECIP >> ./stopped.txt
     echo Subject: process stopped >> ./stopped.txt
@@ -34,12 +34,10 @@ do
     logger 'RMS Pi4 watchdog continuing...'
   fi
   sleepint=20
-  if [ $dayt -lt 11 ] ; then
-    sleepint=600
-  fi
   echo sleeping for $sleepint secs
   sleep $sleepint
 done
 logger 'watchdog exiting...'
 
 exit 0
+
