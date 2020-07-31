504dd993eaf5b9a1f13ab62843c6bdf37ebf1150
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-07-31 11:51:42 +0100

    bugfix to generatemp4s to ensure temp dir cleaned up properly

--- Utils/GenerateMP4s.py
+++ Utils/GenerateMP4s.py
@@ -12,6 +12,7 @@ import subprocess
 import shutil
 import datetime
 import cv2
+import time
 import Utils.FFtoFrames as f2f
 
 from RMS.Formats import FTPdetectinfo
@@ -130,12 +131,19 @@ def GenerateMP4s(dir_path, ftpfile_name):
                     + " -vcodec libx264 -pix_fmt yuv420p -crf 25 -movflags faststart -g 15 -vf \"hqdn3d=4:3:6:4.5,lutyuv=y=gammaval(0.97)\" " \
                     + mp4_path
 
-        print(com)
-        subprocess.call(com, shell=True, cwd=dir_path)
+        #print(com)
+        retcode=subprocess.call(com, shell=True, cwd=dir_path, timeout=None)
         
         #Delete temporary directory and files inside
         if os.path.exists(dir_tmp_path):
-            shutil.rmtree(dir_tmp_path)
+            try:
+                shutil.rmtree(dir_tmp_path)
+            except:
+                # may occasionally fail due to ffmpeg thread still terminating
+                # so catch this and wait a bit
+                time.sleep(2)
+                shutil.rmtree(dir_tmp_path)
+
             print("Deleted temporary directory : " + dir_tmp_path)
 		
     print("Total time:", datetime.datetime.utcnow() - t1)
