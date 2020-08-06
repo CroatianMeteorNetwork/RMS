cbbfa85015e2974e095944a5e2666bb93814f1e1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-08-06 16:35:54 +0100

    add email capability and some sanity checks

--- Scripts/RMS_Pi4Monitor.sh
+++ Scripts/RMS_Pi4Monitor.sh
@@ -3,7 +3,6 @@
 # monitor the logfile and if the capture process stops, restart it
 logger 'RMS pi4 watchdog process starting'
 hn=`hostname`
-MAILRECIP=markmcintyre99@googlemail.com
 
 cd /home/pi/RMS_data/logs
 dead="no"
@@ -18,23 +17,20 @@ do
         logger 'RMS Pi4 watchdog process finished cleanly'
         exit 0
     fi
-    touch /home/pi/source/RMS/.crash
     logger 'RMS Pi4 watchdog RMS stopped acquisition'
     killall python
     sleep 2
     cd ~/source/RMS
     lxterminal -e Scripts/RMS_StartCapture.sh -r
     cd /home/pi/RMS_data/logs
-    echo From: pi@${hn} > ./stopped.txt
-    echo To: $MAILRECIP >> ./stopped.txt
-    echo Subject: process stopped >> ./stopped.txt
-    echo stopped at `date +%Y%m%d-%H%M%S` >> ./stopped.txt
-    echo $logf >>  ./stopped.txt
-    /usr/bin/msmtp -t < ./stopped.txt
     logger 'RMS Pi4 watchdog continuing...'
   fi
+  cd /home/pi/RMS_data/logs
+  acqui=`grep "Starting capture" $fn`
   sleepint=20
-  echo sleeping for $sleepint secs
+  if [ "$acqui" == "" ] ; then
+    sleepint=600
+  fi
   sleep $sleepint
 done
 logger 'watchdog exiting...'
