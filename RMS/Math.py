fixed computing angular separation, optimized levels computation

RMS/Math.py
@@ -8,20 +8,16 @@ def angularSeparation(ra1, dec1, ra2, dec2):
     """ Calculates the angle between two points on a sphere. 
     
     Arguments:
-        dec1: [float] Declination 1 (degrees).
-        ra1: [float] Right ascension 1 (degrees).
-        dec2: [float] Declination 2 (degrees).
-        ra2: [float] Right ascension 2 (degrees).
+        dec1: [float] Declination 1 (radians).
+        ra1: [float] Right ascension 1 (radians).
+        dec2: [float] Declination 2 (radians).
+        ra2: [float] Right ascension 2 (radians).
 
     Return:
-        [float] Angle between two coordinates (degrees).
+        [float] Angle between two coordinates (radians).
     """
-    ra1 = np.radians(ra1)
-    dec1 = np.radians(dec1)
-    ra2 = np.radians(ra2)
-    dec2 = np.radians(dec2)
 
-    return np.degrees(np.arccos(np.sin(dec1)*np.sin(dec2) + np.cos(dec1)*np.cos(dec2)*np.cos(ra2 - ra1)))
+    return np.arccos(np.sin(dec1)*np.sin(dec2) + np.cos(dec1)*np.cos(dec2)*np.cos(ra2 - ra1))
 
 
 def angularSeparationVect(vect1, vect2):

RMS/Routines/AddCelestialGrid.py
@@ -57,8 +57,10 @@ def updateRaDecGrid(grid, platepar):
         az_grid_plot, alt_grid_plot = trueRaDec2ApparentAltAz(ra_grid_plot, dec_grid_plot, platepar.JD, \
             platepar.lat, platepar.lon, platepar.refraction)
 
-        # Filter out points below the horizon  and outside the FOV
-        filter_arr = (alt_grid_plot > 0) & (angularSeparation(alt_centre, azim_centre, alt_grid_plot, az_grid_plot) < fov_radius)
+        # Filter out points below the horizon and outside the FOV
+        filter_arr = (alt_grid_plot > 0) & (np.degrees(angularSeparation(np.radians(alt_centre), \
+            np.radians(azim_centre), np.radians(alt_grid_plot), np.radians(az_grid_plot))) < fov_radius)
+
         ra_grid_plot = ra_grid_plot[filter_arr]
         dec_grid_plot = dec_grid_plot[filter_arr]
 
@@ -83,10 +85,10 @@ def updateRaDecGrid(grid, platepar):
                                                               platepar.lon, platepar.refraction)
 
         # Filter out points below the horizon
-        filter_arr = (alt_grid_plot > 0) #& (angularSeparation(alt_centre,
-                                                              # azim_centre,
-                                                              # alt_grid_plot,
-                                                              # az_grid_plot) < fov_radius)
+        filter_arr = (alt_grid_plot > 0) & (np.degrees(angularSeparation(np.radians(alt_centre),
+                                                              np.radians(azim_centre),
+                                                              np.radians(alt_grid_plot),
+                                                              np.radians(az_grid_plot))) < fov_radius)
         ra_grid_plot = ra_grid_plot[filter_arr]
         dec_grid_plot = dec_grid_plot[filter_arr]
 
@@ -169,10 +171,10 @@ def updateAzAltGrid(grid, platepar):
         alt_grid_plot = np.arange(0, 360, plot_dens)  # how many degrees of circle
         az_grid_plot = np.zeros_like(alt_grid_plot) + az_grid
 
-        # filter_arr = (angularSeparation(alt_centre,
-        #                                 azim_centre,
-        #                                 alt_grid_plot,
-        #                                 az_grid_plot) < fov_radius)
+        # filter_arr = (np.degrees(angularSeparation(np.radians(alt_centre),
+        #                                 np.radians(azim_centre),
+        #                                 np.radians(alt_grid_plot),
+        #                                 np.radians(az_grid_plot))) < fov_radius)
         #
         # alt_grid_plot = alt_grid_plot[filter_arr]
         # az_grid_plot = az_grid_plot[filter_arr]
@@ -194,10 +196,10 @@ def updateAzAltGrid(grid, platepar):
         az_grid_plot = np.arange(0, 90, plot_dens)
         alt_grid_plot = np.zeros_like(az_grid_plot) + alt_grid
 
-        # filter_arr = (angularSeparation(alt_centre,
-        #                                 azim_centre,
-        #                                 alt_grid_plot,
-        #                                 az_grid_plot) < fov_radius)
+        # filter_arr = (np.degrees(angularSeparation(np.radians(alt_centre),
+        #                                 np.radians(azim_centre),
+        #                                 np.radians(alt_grid_plot),
+        #                                 np.radians(az_grid_plot))) < fov_radius)
         #
         # alt_grid_plot = alt_grid_plot[filter_arr]
         # az_grid_plot = az_grid_plot[filter_arr]

RMS/Routines/CustomPyqtgraphClasses.py
@@ -564,25 +564,30 @@ class ImageItem(pg.ImageItem):
         # into a single lut for better performance
         levels = self.levels
 
-        if levels is not None and levels.ndim == 1 and image.dtype in (np.ubyte, np.uint16):
+        if (levels is not None) and (levels.ndim == 1) and (image.dtype in (np.ubyte, np.uint16)):
+
             if self._effectiveLut is None:
+
                 eflsize = 2**(image.itemsize*8)
                 ind = np.arange(eflsize)
                 minlev, maxlev = levels
                 levdiff = maxlev - minlev
                 levdiff = 1 if levdiff == 0 else levdiff  # don't allow division by 0
+
                 if lut is None:
                     efflut = pg.fn.rescaleData(ind, scale=255./levdiff,
                                                offset=minlev, dtype=np.ubyte)
                 else:
                     lutdtype = np.min_scalar_type(lut.shape[0] - 1)
-                    efflut = pg.fn.rescaleData(ind, scale=(lut.shape[0] - 1)/levdiff,
+                    efflut = pg.fn.rescaleData(ind, scale=(lut.shape[0] - 1)/levdiff, \
                                                offset=minlev, dtype=lutdtype, clip=(0, lut.shape[0] - 1))
                     efflut = lut[efflut]
 
                 self._effectiveLut = efflut
+
             lut = self._effectiveLut
             levels = None
+
 
         # Assume images are in column-major order for backward compatibility
         # (most images are in row-major order)
@@ -590,11 +595,22 @@ class ImageItem(pg.ImageItem):
         if self.axisOrder == 'col-major':
             image = image.transpose((1, 0, 2)[:image.ndim])
 
+        # Make an RGB image
         argb, alpha = pg.fn.makeARGB(image, lut=lut, levels=levels)
-        # LINE THAT WAS CHANGED
-        argb[:, :, :3] = np.clip(np.power(argb[:, :, :3]/255, 1/self._gamma)*255, 0, 255)
+        
+        # Perform gamma correction on only one channel to speed things up
+        argb[:, :, 0] = np.clip(np.power(argb[:, :, 0]/255, 1/self._gamma)*255, 0, 255)
+        argb[:, :, 1] = argb[:, :, 0]
+        argb[:, :, 2] = argb[:, :, 0]
+
+        
+        # Invert image colors
         if self.invert_img:
-            argb[:, :, :3] = 255 - argb[:, :, :3]
+            argb[:, :, 0] = 255 - argb[:, :, 0]
+            argb[:, :, 1] = argb[:, :, 0]
+            argb[:, :, 2] = argb[:, :, 0]
+
+
         self.qimage = pg.fn.makeQImage(argb, alpha, transpose=False)
 
 