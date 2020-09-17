2dbc192967605c2a5f99db2f732a4e4cfe83f854
Denis Vida <denis.vida@gmail.com>
2020-09-17 10:21:04 -0400

    formatting

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ kht_module = Extension("kht_module",
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-#init the submodules
-x=subprocess.call(['git','submodule','update','--init'])
+# Init the submodules (python-dvr)
+x = subprocess.call(['git','submodule','update','--init'])
 
 
 ### HANDLE DIFFERENT ONVIF LIBRARIES FOR Py 2 AND 3 ###
