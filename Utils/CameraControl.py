d9a40c902a7aac5eb18fd74c872b58cd64a090fb
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-09-19 00:25:57 +0100

    merging cameracontrol

--- Utils/CameraControl.py
+++ Utils/CameraControl.py
@@ -4,13 +4,9 @@
 
     Note:the DVRIP module used by this code requires Python 3.5 or later.
 
-    usage 1: 
-    Python -m Utils.CameraControl command {opts}
-    call with -h to get a list of supported commands
+    Note:the DVRIP module used by this code requires Python 3.5 or later.
 
-    Usage 2:
-    >>> import Utils.CameraControl as cc
-    >>> cc.CameraControl(ip_address,command, [opts])
+    Note:the DVRIP module used by this code requires Python 3.5 or later.
 
     Parameters:
     command - the command you want to execute. 
@@ -46,6 +42,135 @@ import re
 import RMS.ConfigReader as cr
 from time import sleep
 import git, importlib  #used to import python-dvr as it has a dash in the name
+
+# if not present, force update of the submodule
+try:
+    dvr = importlib.import_module("python-dvr.dvrip")
+except:
+    print("updating python-dvr")
+    rmsloc,_= os.path.split(os.path.realpath(__file__))
+    rmsrepo=git.Repo(rmsloc)
+    for sm in rmsrepo.submodules:
+        sm.update(init=True, force=True)
+    try:
+        dvr = importlib.import_module("python-dvr.dvrip")
+    except:
+        print('unable to update python-dvr - can\'t continue')
+        exit()
+
+def rebootCamera(cam):
+    """Reboot the Camera
+
+    Args:
+        cam : The camera
+    """
+    print('rebooting, please wait....')
+    cam.reboot()
+    sleep(60) # wait while camera starts
+    if cam.login():
+        print('reboot successful')
+    else:
+        print('camera nonresponsive, please wait 30s and reconnect')
+
+def iptoString(s):
+    """Convert an IP address in hex network order to a human readable string 
+
+    Args:
+        s (string): the encoded IP address eg '0x0A01A8C0' 
+
+    Returns:
+        string: human readable IP in host order eg '192.169.1.10'
+    """
+    a=s[2:]
+    addr='0x'+''.join([a[x:x+2] for x in range(0,len(a),2)][::-1])
+    ipaddr=ip.IPv4Address(int(addr,16))
+    return ipaddr
+
+def saveToFile(nc,dh,cs,vs):
+    """Save the camera config to JSON files 
+
+    Args:
+        nc : network config
+        dh : dhcp config
+        cs : camera config
+        vs : video encoding config
+    """
+    if not os.path.exists('./camerasettings/'):
+        os.makedirs('./camerasettings/')
+    with open('./camerasettings/net1.json','w') as f:
+        json.dump(nc,f)
+    with open('./camerasettings/net2.json','w') as f:
+        json.dump(dh,f)
+    with open('./camerasettings/cam.json','w') as f:
+        json.dump(cs,f)
+    with open('./camerasettings/vid.json','w') as f:
+        json.dump(vs,f)
+    print('Settings saved to ./camerasettings/')
+
+
+# if not present, force update of the submodule
+try:
+    dvr = importlib.import_module("python-dvr.dvrip")
+except:
+    print("updating python-dvr")
+    rmsloc,_= os.path.split(os.path.realpath(__file__))
+    rmsrepo=git.Repo(rmsloc)
+    for sm in rmsrepo.submodules:
+        sm.update(init=True, force=True)
+    try:
+        dvr = importlib.import_module("python-dvr.dvrip")
+    except:
+        print('unable to update python-dvr - can\'t continue')
+        exit()
+
+def rebootCamera(cam):
+    """Reboot the Camera
+
+    Args:
+        cam : The camera
+    """
+    print('rebooting, please wait....')
+    cam.reboot()
+    sleep(60) # wait while camera starts
+    if cam.login():
+        print('reboot successful')
+    else:
+        print('camera nonresponsive, please wait 30s and reconnect')
+
+def iptoString(s):
+    """Convert an IP address in hex network order to a human readable string 
+
+    Args:
+        s (string): the encoded IP address eg '0x0A01A8C0' 
+
+    Returns:
+        string: human readable IP in host order eg '192.169.1.10'
+    """
+    a=s[2:]
+    addr='0x'+''.join([a[x:x+2] for x in range(0,len(a),2)][::-1])
+    ipaddr=ip.IPv4Address(int(addr,16))
+    return ipaddr
+
+def saveToFile(nc,dh,cs,vs):
+    """Save the camera config to JSON files 
+
+    Args:
+        nc : network config
+        dh : dhcp config
+        cs : camera config
+        vs : video encoding config
+    """
+    if not os.path.exists('./camerasettings/'):
+        os.makedirs('./camerasettings/')
+    with open('./camerasettings/net1.json','w') as f:
+        json.dump(nc,f)
+    with open('./camerasettings/net2.json','w') as f:
+        json.dump(dh,f)
+    with open('./camerasettings/cam.json','w') as f:
+        json.dump(cs,f)
+    with open('./camerasettings/vid.json','w') as f:
+        json.dump(vs,f)
+    print('Settings saved to ./camerasettings/')
 
 # if not present, force update of the submodule
 try:
