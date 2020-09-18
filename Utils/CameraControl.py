3ccb36d6ae9ea730168863731c2b5de96163878d
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-09-19 00:07:19 +0100

    Added correct CameraControl module

--- Utils/CameraControl.py
+++ Utils/CameraControl.py
@@ -4,13 +4,13 @@
 
     Note:the DVRIP module used by this code requires Python 3.5 or later.
 
-# Usage examples:
-#
-# pip install onvif_zeep
-# python -m RMS.CameraControl reboot
-# python -m RMS.CameraControl getDeviceInformation
+    usage 1: 
+    Python -m Utils.CameraControl command {opts}
+    call with -h to get a list of supported commands
 
-from __future__ import print_function
+    Usage 2:
+    >>> import Utils.CameraControl as cc
+    >>> cc.CameraControl(ip_address,command, [opts])
 
     Parameters:
     command - the command you want to execute. 
@@ -40,6 +40,8 @@ if sys.version_info.major < 3 or (sys.version_info.major > 2 and sys.version_inf
 
 import ipaddress as ip
 import argparse
+import json
+import pprint
 import re
 import RMS.ConfigReader as cr
 from time import sleep
@@ -109,74 +111,287 @@ def saveToFile(nc,dh,cs,vs):
         json.dump(vs,f)
     print('Settings saved to ./camerasettings/')
 
+def loadFromFile():
+    """Load the camera config from JSON files saved earlier
 
-def getHostname(cam):
-    resp = cam.devicemgmt.GetHostname()
-    print('getHostname:\n' + str(resp))
-
-def getDeviceInformation(cam):
-    resp = cam.devicemgmt.GetDeviceInformation()
-    print('getDeviceInformation:\n' + str(resp))
-
-def systemReboot(cam):
-    resp = cam.devicemgmt.SystemReboot()
-    print('systemReboot: ' + str(resp))
-
-
-
-def onvifCommand(config, cmd):
-    """ Execute ONVIF command to the IP camera.
-
-    Arguments:
-        config: [COnfiguration]
-        cmd: [str] Command:
-            - reboot
-            - GetHostname
-            - GetDeviceInformation
     """
+    if not os.path.exists('./camerasettings/'):
+        print('Settings files not found in ./camerasettings/')
+        return 
+    print('Loading settings....')
+    with open('./camerasettings/net1.json','r') as f:
+        nc = json.load(f)
+    with open('./camerasettings/net2.json','r') as f:
+        dh = json.load(f)
+    with open('./camerasettings/cam.json','r') as f:
+        cs = json.load(f)
+    with open('./camerasettings/vid.json','r') as f:
+        vs = json.load(f)
+    print('Loaded')
+    return nc, dh, cs, vs 
 
-    cam = None
+def getNetworkParams(cam, showit=True):
+    """ retrieve or display the camera network settings
 
-    # extract IP from config file
-    camera_ip = re.findall(r"[0-9]+(?:\.[0-9]+){3}", config.deviceID)[0]
-    camera_onvif_port = 8899
+    Args:
+        cam : canera object
+        showit (bool, optional): whether to show the info or just return it. Defaults to True 
 
-    try:
-        print('Connecting to {}:{}'.format(camera_ip, camera_onvif_port))
-        cam = ONVIFCamera(camera_ip, camera_onvif_port, 'admin', '', '/home/pi/vRMS/wsdl')
-    except:
-        print('Could not connect to camera!')
-        exit(1)
+    Returns:
+        json block containing the config
+    """
+    nc=cam.get_info("NetWork.NetCommon")
+    dh=cam.get_info("NetWork.NetDHCP")
 
-    print('Connected.')
+    if showit ==True:
+        pprint.pprint(nc)
+        pprint.pprint(dh)
+        print('IP Address  : ', iptoString(nc['HostIP']))
+        print('Gateway     : ', iptoString(nc['GateWay']))
+    return nc, dh
 
-    # process commands
-    if cmd == 'reboot':
-        systemReboot(cam)
+def getEncodeParams(cam, showit=True):
+    """ Read the Encode section of the camera config
 
+    Args:
+        cam - the camera 
+        saveit (bool, optional): whether we're being called so the data can be saved. Defaults to True.
+
+    Returns:
+        json block containing the config
+    """
+    vidinfo = cam.get_info("Simplify.Encode")
+    if showit == True:
+        pprint.pprint(vidinfo)
+    return vidinfo
+
+def getCameraParams(cam, showit=True):
+    """ display or retrueve the Camera section of the camera config
+
+    Args:
+        cam - the camera 
+        saveit (bool, optional): whether we're being called so the data can be saved. Defaults to True.
+
+    Returns:
+        json block containing the config
+    """
+    caminfo = cam.get_info("Camera")
+    p1 = caminfo['Param'][0]
+    p2 = caminfo['ParamEx'][0]
+    if showit == True:
+        pprint.pprint(p1)
+        pprint.pprint(p2)
+    return caminfo
+
+def setEncodeParam(cam, opts):
+    """ Set a parameter in the Encode section of the camera config
+        Note: a different approach has to be taken here than for camera params
+        as its not possible to set individual parameters without crashing the camera
+    Args:
+        cam - the camera 
+        opts - array of fields, subfields and the value to set
+    """
+    intflds=['FPS','BitRate','GOP','Quality']
+
+    params = cam.get_info("Simplify.Encode")
+    fld=opts[1]
+    if fld == 'Video':
+        subfld=opts[2]
+        val = opts[3]
+        if subfld=='Compression' and val !='H.264' and val != 'H.265':
+            print('Compression must be H.264 or H.265')
+            return 
+        if subfld=='Resolution' and val !='720P' and val != '1080P' and val !='3M':
+            print('Resolution must be 720P, 1080P or 3M')
+            return 
+        if subfld=='BitRateControl' and val !='CBR' and val !='VBR':
+            print('BitRateControl must be VBR or CBR')
+            return
+        if subfld in intflds:
+            val = int(val)
+        params[0]['MainFormat']['Video'][subfld] = val
+    else:
+        val = opts[2]
+        if val!=0 and val!=1:
+            print('AudioEnable and VideoEnable must be 1 or 0')
+            return 
+        subfld=''
+        params[0]['MainFormat'][fld]=val
+    cam.set_info("Simplify.Encode", params)
+    print ('Set {} {} to {}'.format(fld, subfld, val))
+    print ('Camera Reboot required to take effect')
+       
+def setNetworkParam(cam, opts):
+    """ Set a parameter in the Network section of the camera config
+
+    Args:
+        cam - the camera 
+        opts - array of fields, subfields and the value to set
+    """
+    print('Network field setting not implemented yet')
+
+def setCameraParam(cam, opts):
+    """ Set a parameter in the Camera section of the camera config
+        Individual parameters can be set and the change will take effect immediately 
+    Args:
+        cam - the camera 
+        opts - array of fields, subfields and the value to set
+    """
+    # these fields are stored as integers. Others are Hex strings
+    intfields=['AeSensitivity','Day_nfLevel','DncThr','ElecLevel',\
+        'IRCUTMode','IrcutSwap','Night_nfLevel', 'Level','AutoGain','Gain']
+    styleFlds='typedefault','type1','type2'
+    # top level field name
+    fld=opts[1]
+    # these fields are stored in the ParamEx.[0] block
+    if fld == 'Style':
+        val = opts[2]
+        if val not in styleFlds:
+            print('style must be one of ', styleFlds)
+            return
+        cam.set_info("Camera.ParamEx.[0]",{fld:val})
+        print ('Set Camera.ParamEx.[0].{} to {}'.format(fld, val))
+    elif fld == 'BroadTrends': 
+        subfld=opts[2]
+        val = int(opts[3])
+        fldToSet='Camera.ParamEx.[0].'+fld
+        cam.set_info(fldToSet,{subfld:val})
+        print ('Set {} to {}'.format(fldToSet, val))
+                
+    # Exposuretime and gainparam have subfields
+    elif fld == 'ExposureParam' or fld == 'GainParam' :
+        subfld=opts[2]
+        val = int(opts[3])
+        if subfld not in intfields:
+            # the two non-int fields in ExposureParam are the exposure times. 
+            # These are stored in microsconds converted ito hex strings.
+            if val < 100 or val > 80000: 
+                print('Exposure must be between 100 and 80000 microsecs')
+                return
+            val ="0x%8.8X" % (int(val))
+        fldToSet='Camera.Param.[0].'+fld
+        cam.set_info(fldToSet,{subfld:val})
+        print ('Set {} to {}'.format(fldToSet, val))
+    else:
+        # other fields do not have subfields
+        val = int(opts[2])
+        if fld not in intfields:
+            val ="0x%8.8X" % val
+        cam.set_info("Camera.Param.[0]",{fld:val})
+        print ('Set Camera.Param.[0].{} to {}'.format(fld, val))
+
+def setParameter(cam, opts):
+    """ Set a parameter in various sections of the camera config
+
+    Args:
+        cam - the camera 
+        opts - array of fields, subfields and the value to set
+    """
+    if len(opts) < 3:
+        print('Not enough parameters, need at least block, field, value')
+    if opts[0] == 'Camera':
+        setCameraParam(cam, opts)
+    elif opts[0] == 'Encode':
+        setEncodeParam(cam, opts)
+    elif opts[0] == 'Network':
+        setNetworkParam(cam, opts)
+    else:
+        print('Setting not currently supported for', opts)
+
+def dvripCommand(cam, cmd, opts):
+    """ retrieve or display the camera network settings
+
+    Args:
+        cam  - the camera
+        cmd  - the command to execute
+        opts - optional list of parameters to be passed to SetParam
+    """
     if cmd == 'GetHostname':
-        getHostname(cam)
+        nc,_=getNetworkParams(cam, False)
+        print(nc['HostName'])
 
-    if cmd == 'GetDeviceInformation':
-        getDeviceInformation(cam)
+    elif cmd == 'GetNetConfig':
+        getNetworkParams(cam, True)
 
-    exit(0)
+    elif cmd == 'reboot':
+        rebootCamera(cam)
 
+    elif cmd == 'GetCameraParams':
+        getCameraParams(cam, True)
+    
+    elif cmd == 'GetEncodeParams':
+        getEncodeParams(cam, True)
 
+    elif cmd == 'GetSettings':
+        getNetworkParams(cam, True)
+        getCameraParams(cam, True)
+        getEncodeParams(cam, True)
 
+    elif cmd =='SaveSettings':
+        nc, dh = getNetworkParams(cam, False)
+        cs = getCameraParams(cam, False)
+        vs = getEncodeParams(cam, False)
+        saveToFile(nc, dh, cs, vs)
 
+    elif cmd == 'LoadSettings':
+        nc, dh, cs, vs = loadFromFile()
+        cam.set_info("NetWork.NetCommon", nc)
+        cam.set_info("NetWork.NetDHCP", dh)
+        cam.set_info("Camera",cs)
+        cam.set_info("Simplify.Encode", vs)
+        rebootCamera(cam)
+
+    elif cmd == 'SetParam':
+        setParameter(cam, opts)
+
+    else:
+        print('System Info')
+        ugi=cam.get_upgrade_info()
+        print(ugi['Hardware'])
+
+def CameraControl(camera_ip, cmd, opts=''):
+    """CameraControl - main entry point to the module
+
+    Args:
+        camera_ip (string): IPAddress of camera in dotted form eg 192.168.1.10
+        cmd (string): Command to be executed
+        opts (array of strings): Optional array of field, subfield and value for the SetParam command
+    """
+     # Process the IP camera control command
+    cam = dvr.DVRIPCam(camera_ip, "admin", "")
+    if cam.login():
+        try:
+            dvripCommand(cam, cmd, opts)
+        except:
+            print('error executing command - probably not supported')
+    else:
+        print("Failure. Could not connect.")
+    cam.close()
+   
 
 if __name__ == '__main__':
+    """Main function
+    Args:
+        command - the command you want to execute
+        opts - optional list of fields and a value to pass to SetParam
+    """
 
     # list of supported commands
-    cmd_list = ['reboot', 'GetHostname', 'GetDeviceInformation']
+    cmd_list = ['reboot', 'GetHostname', 'GetSettings','GetDeviceInformation','GetNetConfig',  \
+        'GetCameraParams','GetEncodeParams','SetParam','SaveSettings', 'LoadSettings']
+    opthelp='optional parameters for SetParam for example Camera ElecLevel 70 \n' \
+        'will set the AE Ref to 70.\n To see possibilities, execute GetSettings first'
 
-    # command line argument parser
-    parser = argparse.ArgumentParser(description='Controls ONVIF-Compatible IP camera')
+    parser = argparse.ArgumentParser(description='Controls CMS-Compatible IP camera')
     parser.add_argument('command', metavar='command', type=str, nargs=1, help=' | '.join(cmd_list))
+    parser.add_argument('options', metavar='opts', type=str, nargs='*', help=opthelp)
     args = parser.parse_args()
     cmd = args.command[0]
-
+    if args.options is not None:
+        opts = args.options
+    else:
+        opts=''
+        
     if not cmd in cmd_list:
         print('Error: command "{}" not supported'.format(cmd))
         exit(1)
