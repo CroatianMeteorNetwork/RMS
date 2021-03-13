084fac00469254a7e2c75096f187403f37459656
Denis Vida <denis.vida@gmail.com>
2021-03-13 09:35:08 -0500

    fixed samefileerror

--- RMS/Misc.py
+++ RMS/Misc.py
@@ -73,7 +73,10 @@ def archiveDir(source_dir, file_list, dest_dir, compress_file, delete_dest_dir=F
 
     # Copy the files from the source to the archive directory
     for file_name in file_list:
-        shutil.copy2(os.path.join(source_dir, file_name), os.path.join(dest_dir, file_name))
+        try:
+            shutil.copy2(os.path.join(source_dir, file_name), os.path.join(dest_dir, file_name))
+        except shutil.SameFileError:
+            pass
 
     # Copy the additional files to the archive directory
     if extra_files is not None:
