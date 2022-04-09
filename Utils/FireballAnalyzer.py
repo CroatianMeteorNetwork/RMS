1ac130bef52037c3b1659304955d785fd543d69f
Denis Vida <denis.vida@gmail.com>
2022-04-09 17:41:05 -0400

    improved picks

--- Utils/FireballAnalyzer.py
+++ Utils/FireballAnalyzer.py
@@ -103,9 +103,10 @@ if __name__ == "__main__":
 
             # Apply thresholding to the crop image
             threshold_mask = crop_ave + config.k1*crop_std + config.j1
-            threshold_mask[threshold_mask > 250] = 250
+            threshold_mask[threshold_mask > 255] = 255
+            threshold_mask = (crop <= threshold_mask) & (crop < 250)
             crop_masked = np.array(crop)
-            crop_masked[(crop < threshold_mask) & (crop < 250)] = 0
+            crop_masked[threshold_mask] = 0
 
             # Add the image to the summary image
             sum_y0 = max_size*(frame_no//graph_dimension)
@@ -113,63 +114,113 @@ if __name__ == "__main__":
 
             crop_thumb[sum_y0:sum_y0 + 2*(size//2), sum_x0:sum_x0 + 2*(size//2)] = crop_masked
 
+            # Add frame index
+            plt.text(sum_x0, sum_y0, str(frame_index), va='top', ha='left', size=5, color='0.5')
+
 
             # Apply a median filter on the image
             crop_median = scipy.ndimage.median_filter(crop, size=10)
-            crop_median_masked = scipy.ndimage.median_filter(crop_masked, size=10)
+            crop_median_masked = scipy.ndimage.median_filter(crop_masked, size=5)
 
             # Compute the centre of mass and plot it
             #center_mass = scipy.ndimage.measurements.center_of_mass(img_median, labels=(1))
             #y_cmass, x_cmass = center_mass
 
-            # Find the centroid as the peak of the image sum
-            x_sum = np.sum(crop_median_masked, axis=0)
-            y_sum = np.sum(crop_median_masked, axis=1)
-            x_centroid = np.argmax(x_sum)
-            y_centroid = np.argmax(y_sum)
+            if np.any(crop_median_masked):
+
+                # Find the centroid as the peak of the image sum
+                x_sum = np.sum(crop_median_masked, axis=0)
+                y_sum = np.sum(crop_median_masked, axis=1)
+                # x_centroid = np.argmax(x_sum)
+                # y_centroid = np.argmax(y_sum)
+                x_max = np.argwhere(x_sum == np.amax(x_sum)).flatten()
+                x_centroid = x_max[int(round(len(x_max)/2))]
+                y_max = np.argwhere(y_sum == np.amax(y_sum)).flatten()
+                y_centroid = y_max[int(round(len(y_max)/2))]
+
+                automated_x = x_centroid + xc - size/2
+                automated_y = y_centroid + yc - size/2
+
+                automated_picks[frame_index] = (automated_x, automated_y)
+
+                plt.scatter(sum_x0 + x_centroid, sum_y0 + y_centroid, marker='x', c='blue')
+
+            else:
+                x_centroid = None
+                y_centroid = None
+                automated_x = None
+                automated_y = None
 
 
-            automated_x = sum_x0 + x_centroid
-            automated_y = sum_y0 + y_centroid
 
-            automated_picks[frame_index] = (automated_x, automated_y)
+
             
-            if frame_no > 40:
-                plt.clf()
-                plt.plot(x_sum)
-                plt.plot(y_sum)
-                plt.show()
-                print(x_centroid, y_centroid)
+            # if frame_no > 42:
+            #     plt.clf()
+            #     plt.plot(x_sum)
+            #     plt.plot(y_sum)
+            #     plt.vlines(x_centroid, np.min(x_sum), np.max(x_sum))
+            #     plt.vlines(y_centroid, np.min(y_sum), np.max(y_sum))
+            #     plt.show()
+            #     print(x_centroid, y_centroid)
 
-                plt.clf()
-                fig, (ax1, ax2, ax3, ax4) = plt.subplots(ncols=4)
-                ax1.imshow(crop_median, cmap='gray', vmin=0, vmax=255)
-                ax1.scatter(x_centroid, y_centroid, marker='x', c='blue')
-                ax2.imshow(crop_masked, cmap='gray', vmin=0, vmax=255)
-                ax2.scatter(x_centroid, y_centroid, marker='x', c='blue')
+            #     plt.clf()
+            #     fig, (ax0, ax1, ax2, ax3, ax4, ax5) = plt.subplots(ncols=6, sharex=True, sharey=True)
+            #     ax0.imshow(crop, cmap='gray', vmin=0, vmax=255)
+            #     ax0.scatter(x_centroid, y_centroid, marker='x', c='blue')
+            #     ax1.imshow(crop_median, cmap='gray', vmin=0, vmax=255)
+            #     ax1.scatter(x_centroid, y_centroid, marker='x', c='blue')
+            #     ax2.imshow(crop_masked, cmap='gray', vmin=0, vmax=255)
+            #     ax2.scatter(x_centroid, y_centroid, marker='x', c='blue')
+            #     ax3.imshow(crop_median_masked, cmap='gray', vmin=0, vmax=255)
+            #     ax3.scatter(x_centroid, y_centroid, marker='x', c='blue')
 
-                ax3.imshow(crop_ave, cmap='gray', vmin=0, vmax=255)
-                ax4.imshow(crop_std, cmap='gray', vmin=0, vmax=255)
-                plt.show()
-
-            plt.scatter(sum_x0 + x_centroid, sum_y0 + y_centroid, marker='x', c='blue')
+            #     ax4.imshow(crop_ave, cmap='gray', vmin=0, vmax=255)
+            #     ax5.imshow(crop_std, cmap='gray', vmin=0, vmax=255)
+            #     plt.show()
 
 
             # Find FTPdetectinfo pick and plot it
             for pick in meteor_data[0][11]:
-                calib_status, frame_n, x, y, ra, dec, azim, elev, inten, mag = pick
+                calib_status, frame_n, manual_x, manual_y, ra, dec, azim, elev, inten, mag = pick
 
-                manual_x = sum_x0 + x - xc + size/2
-                manual_y = sum_y0 + y - yc + size/2
+                manual_thumb_x = sum_x0 + manual_x - xc + size/2
+                manual_thumb_y = sum_y0 + manual_y - yc + size/2
 
                 manual_picks[frame_index] = (manual_x, manual_y)
 
                 if int(frame_index) == int(frame_n):
-                    plt.scatter(manual_x, manual_y, marker='+', c='red')
+                    plt.scatter(manual_thumb_x, manual_thumb_y, marker='+', c='red')
+
+
+                    print("automated:", automated_x, automated_y)
+                    print("manual:", manual_x, manual_y)
+
+                    break
 
 
 
 
         plt.imshow(crop_thumb, cmap='gray')
+
+        plt.show()
+
+
+        # Plot deviations per frame
+        for frame_index in sorted(automated_picks.keys()):
+
+            automated_x, automated_y = automated_picks[frame_index]
+            manual_x, manual_y = manual_picks[frame_index]
+
+            if automated_x is not None:
+                x_diff = manual_x - automated_x
+                y_diff = manual_y - automated_y
+                plt.scatter(frame_index, x_diff, marker='x', color='b')
+                plt.scatter(frame_index, y_diff, marker='+', color='r')
+
+                print(frame_index)
+                print("    automated:", automated_x, automated_y)
+                print("    manual:", manual_x, manual_y)
+
 
         plt.show()
