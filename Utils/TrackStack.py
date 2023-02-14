30406813e7c4b277cbf1eb584b51f06b9ffe0591
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2023-02-14 07:54:08 +0100

    Use computed field of view for constellations

--- Utils/TrackStack.py
+++ Utils/TrackStack.py
@@ -355,7 +355,8 @@ def trackStack(dir_paths, config, border=5, background_compensation=True,
 
     # Draw constellations
     if draw_constellations:
-        constellations_img = drawConstellations(pp_stack, ff_mid)
+        constellations_img = drawConstellations(pp_stack, ff_mid,
+                                                separation_deg=ang_sep_max)
 
 
     # Crop image
