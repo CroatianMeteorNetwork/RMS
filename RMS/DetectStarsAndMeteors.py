8b8cdc29037bd58eb671fd6c30e7c99f9e74dbbd
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-05-16 13:11:19 +0100

    removing unused brightness logging

--- RMS/DetectStarsAndMeteors.py
+++ RMS/DetectStarsAndMeteors.py
@@ -98,13 +98,6 @@ def detectStarsAndMeteors(ff_directory, ff_name, config, flat_struct=None, dark=
         meteor_list = detectMeteors(img_handle, config, flat_struct=flat_struct, dark=dark, mask=mask)
 
         log.info(ff_name + ' detected meteors: ' + str(len(meteor_list)))
-        if len(meteor_list) > 0:
-            max_intensity = 0.0
-            for met in meteor_list:
-                for cen in met[2]:
-                    max_intensity = max(max_intensity, cen[3])
-            log.info(msg = '{} max intensity {:.4f}'.format(ff_name, max_intensity))
-
     else:
         meteor_list = []
 
