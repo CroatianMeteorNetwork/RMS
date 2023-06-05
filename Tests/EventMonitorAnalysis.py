d1a09611bf345bfcb3d9d4739c352669cfcf4fef
David Rollinson <g7gpr@outlook.com>
2023-06-05 03:54:04 +0000

    Fixed looping error in Tests

---
+++ Tests/EventMonitorAnalysis.py
@@ -0,0 +1,96 @@
+""""" Automatically uploads data files based on time and trajectory information given on a website. """
+
+from __future__ import print_function, division, absolute_import
+
+import numpy as np
+
+from RMS.EventMonitor import EventContainer, EventMonitor, convertgmntimetoposix
+import RMS.ConfigReader as cr
+
+import os
+import json
+import time
+import datetime
+import argparse
+import pathlib
+from RMS.Formats.Platepar import Platepar
+from datetime import datetime
+from dateutil import parser
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+
+def analysetestresults(config):
+
+    #build the dataframe
+
+    allcameras= []
+    with open(os.path.expanduser(os.path.join(config.data_dir, "testlog")), 'rt') as logfile:
+        readingheader=True
+        body = header = ""
+        for line in logfile:
+            readingheader = True if 'GMN' in line else False
+            header = line.replace("'","").replace(" ","") if readingheader else header
+            body = line if not readingheader else body
+            cameralist = header.split('[')[1].split(']')[0].split(',')
+
+            for camera in cameralist:
+                if camera not in allcameras:
+                    allcameras.append(camera)
+
+    # Build the data frame for the analysis
+
+    allcameras.sort()
+
+    catlist = ['Used', 'Predicted', 'Predicted and used', 'Predicted not used', 'Used not Predicted',
+               'Not used Not predicted']
+
+    df = pd.DataFrame(data = allcameras)
+    df.rename(columns={0:'Camera'}, inplace=True)
+
+    for category in catlist:
+        df[category] = 0
+
+    print(df)
+
+    allcameras= []
+    with open(os.path.expanduser(os.path.join(config.data_dir, "testlog")), 'rt') as logfile:
+        readingheader=True
+        body = header = ""
+        for line in logfile:
+            readingheader = True if 'GMN' in line else False
+            header = line.replace("'","").replace(" ","") if readingheader else header
+            body = line if not readingheader else body
+            if readingheader:
+                predictedcameras = []
+                usedcameralist = header.split('[')[1].split(']')[0].split(',')
+                df.loc[df['Camera'].isin(usedcameralist), 'Used'] = df.loc[df['Camera'].isin(usedcameralist), 'Used'] + 1
+
+            if not readingheader:
+
+                predictedcamera = body.split(" ")[3]
+
+                df.loc[df['Camera'].isin([predictedcamera]), 'Predicted'] = df.loc[df['Camera'].isin([predictedcamera]), 'Predicted'] + 1
+
+                pass
+
+
+
+
+
+    print("Results")
+    print(df.to_string())
+
+if __name__ == "__main__":
+
+
+    arg_parser = argparse.ArgumentParser(description="""Analyse test results from EventMonitor tests. \
+        """, formatter_class=argparse.RawTextHelpFormatter)
+
+    arg_parser.add_argument('-c', '--config', nargs=1, metavar='CONFIG_PATH', type=str, \
+                            help="Path to a config file which will be used instead of the default one.")
+
+    cml_args = arg_parser.parse_args()
+
+    config = cr.loadConfigFromDirectory(cml_args.config, os.path.abspath('eventmonitor'))
+    analysetestresults(config)
