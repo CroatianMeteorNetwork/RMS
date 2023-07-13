8ec5e95faf4a9f7b4008ca9b4890e7b99bc4310b
David Rollinson <g7gpr@outlook.com>
2023-07-13 12:13:13 +0000

    Correct typo in comment

--- RMS/Astrometry/Conversions.py
+++ RMS/Astrometry/Conversions.py
@@ -618,7 +618,7 @@ def AER2LatLonAlt(azim, elev, r, lat, lon, alt):
         alt: [float] Altitude of observer in meters.
 
     Return:
-        (lat, lon, alt): [tuple of floats] range in meteors, latitude and longitude in degrees, 
+        (lat, lon, alt): [tuple of floats] range in meters, latitude and longitude in degrees,
             WGS84 elevation in meters
 
     """
