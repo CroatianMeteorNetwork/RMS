f2125a12e3e2e11f62ed96747730bdace898a2ff
g7gpr <g7gpr@outlook.com>
2023-07-26 06:35:23 +0000

    Correct argument order

--- RMS/EventMonitor.py
+++ RMS/EventMonitor.py
@@ -1533,6 +1533,8 @@ class EventMonitor(multiprocessing.Process):
             if file.endswith(".bin"):
                 ff_file = convertFRNameToFF(os.path.basename(file))
                 try:
+                    log.info("this_event_directory {}".format(this_event_directory))
+                    log.info("ff_file {}, fr_file {}".format(ff_file, fr_file))
                     view(this_event_directory, ff_file, file, self.syscon, hide=True, add_timestamp=True, extract_format="mp4")
                 except:
                     log.error("Converting {} to mp4 failed".format(file))
