cae820d46f2733b365ddd945ff2975360316de75
David Rollinson <g7gpr@outlook.com>
2023-08-30 08:18:34 +0000

    Formatting

--- RMS/EventMonitor.py
+++ RMS/EventMonitor.py
@@ -14,11 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# A full event definition is shown below
-
-
-
-
 
 from __future__ import print_function, division, absolute_import
 
