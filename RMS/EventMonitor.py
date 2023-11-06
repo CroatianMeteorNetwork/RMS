ffc03370143967f74f17ec0dbfa1699fa9a3b738
David Rollinson <g7gpr@outlook.com>
2023-11-06 12:10:14 +0000

    Add tle_last_processed to db

--- RMS/EventMonitor.py
+++ RMS/EventMonitor.py
@@ -2602,16 +2602,21 @@ class EventMonitor(multiprocessing.Process):
                 night_directory_list.append(night_directory)
         else:
             log.warning("Could not find CapturedFiles directory")
+            return
 
-        log.info("Night directory list")
+        if len(night_directory_list) == 0:
+            log.info("Night directory list is empty")
+            return
 
-
-
-        if event.tle_last_processed == "":
+        if event.tle_last_processed == "" and len(night_directory_list) != 0:
             night_directory_list.sort()
+            log.info("{}".format(night_directory_list))
             directory_to_evaluate = night_directory_list[0]
             log.info("This TLE has not had any processing, pick the earliest directory")
             log.info("{}".format(directory_to_evaluate))
+
+
+
 
 
 
