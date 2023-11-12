64c2d1812dcebfb3353b3856864adfc2ac3bd025
David Rollinson <g7gpr@outlook.com>
2023-11-12 12:00:49 +0000

    Additional logging

--- Utils/MultiLapse.py
+++ Utils/MultiLapse.py
@@ -175,7 +175,8 @@ def camStack(config_path_list, stack_time = datetime.datetime.utcnow() - datetim
                         else:
                             if abs((file_time - stack_time).total_seconds()) < smallest_delta_seconds:
                                 closest_file = file
-                                print(closest_file)
+                                print("file         :{}".format(file))
+                                print("closest file :{}".format(closest_file))
                                 smallest_delta_seconds = abs((file_time - stack_time).total_seconds())
         print("Closest file is ".format(closest_file))
         matching_fits_list.append(os.path.join(directory,closest_file))
