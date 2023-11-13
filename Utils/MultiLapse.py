1b8ce56e68db606a8332745820fa7ab3e5eb8684
David Rollinson <g7gpr@outlook.com>
2023-11-13 12:31:32 +0000

    Round filter

--- Utils/MultiLapse.py
+++ Utils/MultiLapse.py
@@ -253,7 +253,7 @@ def camStack(config_path_list, stack_time = datetime.datetime.utcnow() - datetim
         # filter_arr = (stack_x > 0) & (stack_x < img_size) & (stack_y > 0) & (stack_y < img_size)
         img_cent = img_size / 2
         # try to make a round filter
-        filter_arr =  (0.3 * img_size) ** 2 < ((img_cent - stack_x) ** 2 + (img_cent - stack_y) ** 2)
+        filter_arr =  ((0.3 * img_size) ** 2) < ((img_cent - stack_x) ** 2 + (img_cent - stack_y) ** 2)
         x_coords = x_coords[filter_arr].astype(int)
         y_coords = y_coords[filter_arr].astype(int)
         stack_x = stack_x[filter_arr]
