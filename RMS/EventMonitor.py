fe6afd67e87ab5e1d1d506f81fd12abd1b10b40c
David Rollinson <g7gpr@outlook.com>
2023-12-05 10:56:12 +0000

    Force redownload of sun ephemeris if exception

--- RMS/EventMonitor.py
+++ RMS/EventMonitor.py
@@ -32,7 +32,7 @@ import copy
 import uuid
 import random
 import string
-from skyfield.api import EarthSatellite, load, wgs84
+from skyfield.api import EarthSatellite, load, wgs84, Loader
 from skyfield.timelib import Time, utc
 
 
@@ -2754,7 +2754,8 @@ class EventMonitor(multiprocessing.Process):
             except:
                 log.info("Creating a trajectory for {} between {} and {}".format(event.tle_0, start_time_gmn, end_time_gmn))
                 log.error("Error trying to evaluate if {} was sunlit at {}".format(event.tle_0, evaluation_time))
-
+                log.info("Force redownload of sun ephemeris")
+                eph_sun =  Loader.download('de421.bsp')
         return event
 
     def checkTLEEvent(self, tle_event, ev_con, test_mode = False):
