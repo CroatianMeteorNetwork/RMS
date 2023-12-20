6ea0687ac362bb7be8cc119b2dc3d7fe90637e42
David Rollinson <g7gpr@outlook.com>
2023-12-20 22:30:15 +0000

    Correct config attributes

--- RMS/DownloadMask.py
+++ RMS/DownloadMask.py
@@ -99,7 +99,7 @@ def downloadNewMask(config, port=22):
 
 
     # Download the remote mask
-    sftp.get(remote_mask, os.path.join(config.config_file_path, config.mask_name))
+    sftp.get(remote_mask, os.path.join(config.config_file_path, config.mask_file))
 
     log.info('Latest mask downloaded!')
 
