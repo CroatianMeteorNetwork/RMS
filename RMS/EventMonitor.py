1aa6a246b723c3baa7dce6197f3612841b4dc0fb
David Rollinson <g7gpr@outlook.com>
2024-02-11 07:31:03 +0000

    Restore sleep at start

--- RMS/EventMonitor.py
+++ RMS/EventMonitor.py
@@ -2114,7 +2114,7 @@ class EventMonitor(multiprocessing.Process):
         # Delay to allow capture to check existing folders - keep the logs tidy
 
 
-        time.sleep(10)
+        time.sleep(60)
         last_check_start_time = datetime.datetime.utcnow()
         while not self.exit.is_set():
             check_start_time = datetime.datetime.utcnow()
