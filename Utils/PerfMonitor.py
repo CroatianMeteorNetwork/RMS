00e44f56f7e912c64134744e16d60db7888db2c6
Cybis320 <luc.busquin@gmail.com>
2024-03-29 09:25:32 -0700

    Add model info

--- Utils/PerfMonitor.py
+++ Utils/PerfMonitor.py
@@ -15,7 +15,7 @@ class PerfMonitor:
         self.fieldnames = ['data_dir_name', 'write_speed_mbps', 'res', 'calc_fps', 'media_backend', 
                            'media_backend_ovr', 'live_maxpixel', 'live_jpg', 'slideshow', 'hdu_compress', 
                            'fireball_detection', 'jitter_quality', 'dropped_frame_rate', 'os_version', 
-                           'architecture', 'cpu']
+                           'architecture', 'model']
         self.night_data_dir_name = night_data_dir_name
         self.lock = Lock()
 
@@ -28,10 +28,10 @@ class PerfMonitor:
             else:
                 # Retrieve the existing entry to modify it
                 entry = self.data_entries[self.night_data_dir_name]
-            
+
             # Update the entry
             entry[key] = value
-            
+
             # Re-assign the modified entry back to the managed dictionary
             self.data_entries[self.night_data_dir_name] = entry
 
@@ -39,20 +39,20 @@ class PerfMonitor:
     def logToCsv(self):
         # Check if the file exists to determine if we need to write headers
         file_exists = os.path.isfile(self.log_file_path)
-        
+
         with self.lock:
             with open(self.log_file_path, 'a', newline='') as csvfile:
                 writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
-                
+
                 # Write the header only if the file did not exist before
                 if not file_exists:
                     writer.writeheader()
-                    
+
                 for data_dir, entry in self.data_entries.items():
                     # Ensure 'data_dir_name' is part of the entry for logging
                     entry['data_dir_name'] = data_dir
                     writer.writerow(entry)
-                    
+
         # Clear entries after logging
         self.data_entries.clear()
 
@@ -113,3 +113,25 @@ class PerfMonitor:
         self.updateEntry('cpu', info['cpu'])
 
         return info
+
+
+    def getModel(self):
+        try:
+            with open("/proc/device-tree/model", "r") as model_file:
+                # Read the model information
+                model_info = model_file.read().strip()
+
+                # Log the result
+                self.updateEntry('model', model_info)
+
+                return model_info
+        except IOError:
+            try:
+                with open("/sys/firmware/devicetree/base/model", "r") as model_file:
+                    model_info = model_file.read().strip()
+                    return model_info
+            except IOError:
+                pass  # Model file could not be read
+
+        return "Unknown Model"
+
