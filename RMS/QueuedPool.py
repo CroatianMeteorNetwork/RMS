f425107c48e451b8418e0a7df701383e7ff7c07c
Denis Vida <denis.vida@gmail.com>
2024-07-08 09:58:11 -0400

    added a check on pickle loading state

--- RMS/QueuedPool.py
+++ RMS/QueuedPool.py
@@ -244,7 +244,12 @@ class QueuedPool(object):
         for file_name in self._listBackupFiles():
 
             # Load the backup file
-            bkup_obj = loadPickle(self.bkup_dir, file_name)
+            try:
+                bkup_obj = loadPickle(self.bkup_dir, file_name)
+            except Exception as e:
+                self.printAndLog('Failed loading backup file:', file_name)
+                self.printAndLog(repr(e))
+                continue
 
             if bkup_obj is None:
                 continue
