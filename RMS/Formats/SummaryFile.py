24481635d80ec742e3a9f01bc946f803cef1dc74
g7gpr <g7gpr@outlook.com>
2024-07-30 06:43:25 +0800

    Update license and add template

--- RMS/Formats/SummaryFile.py
+++ RMS/Formats/SummaryFile.py
@@ -3,7 +3,7 @@
 
 # The MIT License
 
-# Copyright (c) 2016 Denis Vida
+# Copyright (c) 2024
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,3 +22,34 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
+"""
+
+Creates a text file summarising key station information and observation session
+
+date                    :   Mon Jul 22 10:47:01 2024 -0400    
+commit                  :   3a20e04d8fe8104b02af82b3fc34d3e00ace2bd4 (HEAD -> master, origin/master, origin/HEAD, dalava/master)                       
+merge                   :   344c26 2aee258           
+hardware_version        :   Raspberry Pi 4 Model B Rev 1.1    
+os_version              :   Debian GNU/Linux 11 (bullseye)      ; operating system version
+storage_space           :   117G       
+fits_files              :   3079           
+fits_missing            :   0         
+minutes_missing         :   0   
+storage_status          :   4
+detection_before_ml     :   52
+detections_after_ml     :   43        
+number_fits_detected    :   37
+photometry_status       :   good 
+dropped_frames          :   2  
+jitter_quality          :   100.0%  
+dropped_frame_rate      :   0.0%
+lens_type               :   4mm
+media_backend           :   gst
+
+"""
+
+
+
+
+
