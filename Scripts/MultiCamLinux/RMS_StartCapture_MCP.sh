923d285c5a1071a8abbe57357076f7eca237c7e9
g7gpr <g7gpr@outlook.com>
2024-08-14 12:16:02 +0800

    Revert typo

--- Scripts/MultiCamLinux/RMS_StartCapture_MCP.sh
+++ Scripts/MultiCamLinux/RMS_StartCapture_MCP.sh
@@ -5,6 +5,6 @@ for Dir in ~/source/Stations/*
   do
 	Station=$(basename $Dir)
 	echo "Starting camera ${Station}"
-	lxterminal --title=${Station} -c "~/source/RMS/Scripts/MultiCamLinux/StartCapture.sh ${Station}"  &
+	lxterminal --title=${Station} -e "~/source/RMS/Scripts/MultiCamLinux/StartCapture.sh ${Station}"  &
 	sleep 30
   done
