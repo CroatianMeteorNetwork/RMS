79ae32604f3d6be2cc54a1bbc2b9b2f7c6032d1e
David Rollinson <g7gpr@outlook.com>
2024-08-16 15:00:51 +0000

    Initial commit of UpdateConfig

---
+++ Utils/UpdateConfig.py
@@ -0,0 +1,444 @@
+import configparser
+import argparse
+import os
+from Utils.AuditConfig import validatePath
+import glob
+import shutil
+import tempfile
+from RMS.Misc import mkdirP
+
+def parseConfigFileBySection(config_path):
+    """Parse the .config file, into one list and two lists of lists
+
+
+    Arguments:
+        config_path: [str] Path to the .config file
+
+    Returns:
+        sections_list: A list of all the sections
+        options_list_of_lists: A list of lists of all the options
+        values_list_of_lists: A list of lists of all the values
+
+    """
+
+    print("Reading in configuration from {:s}".format(config_path))
+
+    config = configparser.ConfigParser()
+    config.read(os.path.expanduser(config_path))
+
+    sections_list = []
+    options_list_of_lists = []
+    values_list_of_lists = []
+
+    for section in config.sections():
+        sections_list.append(section)
+        options_list = []
+        values_list = []
+        for option in config.options(section):
+            if option:
+                options_list.append(option)
+                value = config.get(section,option)
+                if ";" in value:
+                    value = value[:value.index(";")].strip()
+                values_list.append(config.get(section,option))
+        options_list_of_lists.append(options_list)
+        values_list_of_lists.append(values_list)
+
+    return sections_list, options_list_of_lists, values_list_of_lists
+
+def backupConfig(config_file_path, number_to_keep=5):
+
+    """Create numbered backups of the .config file
+
+
+        Arguments:
+            config_path: [str] Path to the .config file
+
+        Returns:
+            sections_list: A list of all the sections
+            options_list_of_lists: A list of lists of all the options
+            values_list_of_lists: A list of lists of all the values
+
+        """
+
+    config_file_backups = sorted(glob.glob(config_file_path + "_*"), reverse=True)
+    for config_backup in config_file_backups:
+        name, number = config_backup.split("_")[0], config_backup.split("_")[1]
+
+        number = int(number)
+        if int(number) < number_to_keep:
+            number += 1
+            os.rename(config_backup,"{}_{}".format(name,number))
+        else:
+            os.rename(config_backup,"{}_{}".format(name,number))
+
+    config_backup = config_file_path +"_1"
+    shutil.copy(config_file_path, config_file_path +"_1")
+
+    return config_backup
+
+
+
+def sectionHeaderLine(line, section_list):
+
+    """
+    Detect if this is a section header line
+
+    Arguments:
+        line: line to be checked
+        section_list: list of all the expected sections
+
+    Returns:
+        bool
+
+    """
+
+
+    opening_square_bracket, closing_square_bracket = False, False
+    opening_square_bracket_position, closing_square_bracket_position = 0, 0
+
+    n = 0
+    for c in line:
+        if c == "[" and opening_square_bracket_position == False:
+            opening_square_bracket = True
+            opening_square_bracket_position = n
+        if c == "]" and opening_square_bracket:
+            closing_square_bracket = True
+            closing_square_bracket_position = n
+        n += 1
+    if opening_square_bracket and closing_square_bracket:
+        header = line[opening_square_bracket_position + 1:closing_square_bracket_position]
+        if header in section_list:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def getOption(line, delimiter =":"):
+
+    """
+    Get the option, if any from a line
+
+    Arguments:
+        line: line to be checked
+        delimiter: optional default :
+
+    Returns:
+        str: option name
+
+    """
+
+    if delimiter in line:
+        return line[0:line.index(delimiter)].strip().lower()
+    else:
+        return None
+
+def getValue(line, delimiter = ":"):
+
+
+    """
+    Get the value, if any from a line
+
+    Arguments:
+        line: line to be checked
+        delimiter: optional default :
+
+    Returns:
+        str: value
+
+    """
+
+
+    if delimiter in line:
+        return line[line.index(delimiter) + 1:].strip()
+    else:
+        return None
+
+
+
+def insert(current_section, insert_options_list, interactive=False):
+
+
+    """
+    Create a string containing options which can be inserted into the .config file
+
+    Arguments:
+        line: line to be checked
+        delimiter: optional default :
+
+    Returns:
+        str: value
+
+    """
+
+    output_lines = ""
+
+    for section, option, value in insert_options_list:
+        if section == current_section:
+
+            insert = "{}: {}\n".format(option, value)
+            if interactive:
+                print("Working in section {}".format(section))
+                response = input("Would you like to insert {}".format(insert))
+                if response.lower() == "y":
+                    output_lines += insert
+            else:
+                output_lines += insert
+
+    return output_lines
+
+def populateTemplateConfig(config_template_file_path, config_file_path,
+                            sections_list, options_list_of_lists, values_list_of_lists,
+                                 temporary_suffix="new", insert_options_list = [], interactive = False):
+
+
+    """
+    Reads in a .config template file, and populates it with the data collected from another .config file
+    If an option is commented out in the template, but is required in the station config file, then it
+    will be uncommented, provided it does not exist elsewhere.
+
+    Arguments:
+        config_template_file_path: path to the template file
+        config_file_path: path to the config file t be upgradedd
+        sections_list: list of the sections in the original config file
+        options_list_of_lists: list of lists of options
+        values_list_of_lists: list of lists of values
+        temporary_suffix: optional suffix to use for the temporary config file
+        insert_options_list: list of [section, option, value] to insert, used if the template is missing required
+                                options
+        interactive: optional, default False, if True, asks for confirmations
+
+    Returns:
+        str: value
+
+    """
+
+
+
+    config_template_file_path = os.path.expanduser(config_template_file_path)
+    config_file_path = os.path.expanduser(config_file_path)
+    config_template_file_name = os.path.basename(config_template_file_path)
+    config_file_name = os.path.basename(config_file_path)
+
+    temp_directory = tempfile.mkdtemp()
+    if os.path.isfile(temp_directory):
+        os.unlink(temp_directory)
+    mkdirP(temp_directory)
+
+    shutil.copy(config_file_path, os.path.join(temp_directory, config_template_file_name))
+    shutil.copy(config_template_file_path, os.path.join(temp_directory, config_template_file_name))
+    temporary_template = os.path.join(temp_directory, os.path.basename(config_template_file_path))
+    temporary_output_file = os.path.join(temp_directory, "{}_{}".format(config_file_name,temporary_suffix))
+
+    input_file_handle = open(temporary_template, "r")
+    output_file_handle = open(temporary_output_file,"w")
+
+    lines_list = []
+    for line in input_file_handle:
+        lines_list.append(line)
+
+    current_section = ""
+    for line in lines_list:
+        output_line = ""
+        blank_line, comment_line, section_header_line, uncomment_line = False, False, False, False
+        line = line.replace("\n","")
+        if not len(line):
+            blank_line = True
+        elif line.strip()[0] == ";" or line.strip()[0] == "#":
+            comment = line.strip()[1:].strip()
+            comment_line = True
+            # Could this be an option that is commented out
+            if ":" in comment:
+                if current_section in sections_list:
+                    option = comment[:comment.index(":")]
+                    if option in options_list_of_lists[sections_list.index(current_section)]:
+                        uncomment_line = True
+                        # This option is required in the station configuration file.
+                        # Does it exist elsewhere uncommented?
+                        for line2 in lines_list:
+                            if option in line2:
+                                if option == line2[:len(option)]:
+                                    # It exists elsewhere uncommented, do not uncomment this line
+                                    uncomment_line = False
+            else:
+                comment_line = True
+        elif sectionHeaderLine(line, sections_list):
+            section_header_line = True
+
+        if section_header_line:
+            if current_section != "":
+                # This is the last line of this section, so insert any missing options
+                output_line += insert(current_section, insert_options_list, interactive=interactive)
+            current_section = line.strip()[1:-1]
+
+
+        if uncomment_line:
+            option_number = options_list_of_lists[sections_list.index(current_section)].index(option)
+            value = values_list_of_lists[sections_list.index(current_section)][option_number]
+            line = "{}: {}".format(option, value)
+
+        if not blank_line and not comment_line and not section_header_line and not uncomment_line:
+            option = getOption(line)
+            section_number = sections_list.index(current_section)
+            if option.lower() in options_list_of_lists[section_number]:
+                option_number = options_list_of_lists[section_number].index(option.lower())
+                station_value = values_list_of_lists[section_number][option_number]
+                output_line = "{}: {}\n".format(option, station_value)
+            else:
+                if interactive:
+                    print("Option:{} from Section:{} was not found in station .config, set to {}"
+                                                .format(option, current_section, getValue(line)))
+                output_line = line
+
+        else:
+            output_line += "{}\n".format(line)
+
+        output_file_handle.write(output_line)
+    input_file_handle.close()
+    output_file_handle.close()
+
+    return temporary_output_file
+
+def compare(new_config_path, sections_list, options_list_of_lists, values_list_of_lists):
+
+    """
+    Compares a configuration file on disc against
+
+    """
+
+
+    fh = open(new_config_path,"r")
+    config_file_as_list, errors, missing_options = [], [], []
+
+
+    for line in fh:
+        config_file_as_list.append(line.replace("\n",""))
+
+    correct_section = False
+    for section in sections_list:
+        for option, value in zip(options_list_of_lists[sections_list.index(section)], values_list_of_lists[sections_list.index(section)]):
+            correct_section, option_value_mismatch, found_option_value = False, False, False
+            wrong_value = ""
+
+            for line in config_file_as_list:
+                if sectionHeaderLine(line, sections_list):
+                    correct_section = True if line.strip() == "[{}]".format(section) else False
+
+                if correct_section:
+
+                    if getOption(line) == option and getValue(line) != value:
+                        option_value_mismatch = True
+                        found_option_value = True
+                        wrong_value = getValue(line)
+                        break
+
+                    if getOption(line) == option and getValue(line) == value:
+                        found_option_value = True
+
+            if option_value_mismatch and found_option_value:
+                errors.append("Found {} in section {} with wrong value {} instead of {}".
+                                    format(option, section, wrong_value, value))
+
+            if not found_option_value:
+                errors.append("Did not find option {} in section {} with expected value {}".format(option, section, value))
+                missing_options.append([section,option,value])
+
+    return errors, missing_options
+
+
+def updateConfig(config_file_path, config_template_file_path, interactive=False):
+
+
+
+
+    config_file_path = os.path.expanduser(config_file_path)
+    config_template_file_path = os.path.expanduser(config_template_file_path)
+
+    if validatePath(config_file_path, ".config"):
+        # Make backup of the .config file
+        config_backup = backupConfig(config_file_path)
+        # Parse the .config file
+        sections_list, options_list_of_lists, values_list_of_lists = parseConfigFileBySection(config_file_path)
+        # Create a new config file in a temporary directory
+        new_config_path = populateTemplateConfig(config_template_file_path, config_file_path, sections_list,
+                                                                options_list_of_lists, values_list_of_lists,
+                                                                interactive=interactive)
+
+        # Check that every value that was in the station .config file has been migrated in the new .config file
+        error_list, missing_option_list = compare(new_config_path, sections_list, options_list_of_lists,
+                                                  values_list_of_lists)
+
+        if interactive:
+            abort = False
+            while len(error_list) or abort:
+                print("Unsuccessful compare - the following missing options were noted")
+                for error in error_list:
+                    print("    {:s}".format(error))
+                response = input("Would you like to insert the missing options? (y/n)")
+                if response.lower() == "y":
+                    new_config_path = populateTemplateConfig(config_template_file_path, config_file_path, sections_list,
+                                                         options_list_of_lists, values_list_of_lists,
+                                                         insert_options_list=missing_option_list,
+                                                             interactive=interactive)
+
+                    error_list, missing_option_list = compare(new_config_path, sections_list, options_list_of_lists,
+                                                      values_list_of_lists)
+                else:
+                    break
+
+
+            print("The new configuration is stored at {}".format(new_config_path))
+            print("A backup of the .config file is stored at {}".format(config_backup))
+            response = input("Copy new config file at {} over old config file at {} (y/n)"
+                                            .format(new_config_path, config_file_path))
+            if response.lower() == "y":
+                shutil.copy(new_config_path, config_file_path)
+
+            response = input("Would you like remove the temporary working directory (y/n)")
+            if response.lower() == "y":
+                files = os.listdir(os.path.dirname(new_config_path))
+                for file in files:
+                    os.unlink(os.path.join(os.path.dirname(new_config_path), file))
+                os.rmdir(os.path.dirname(new_config_path))
+                exit(0)
+            else:
+                exit(0)
+
+        else:
+            new_config_path = populateTemplateConfig(config_template_file_path, config_file_path, sections_list,
+                                                     options_list_of_lists, values_list_of_lists,
+                                                     insert_options_list=missing_option_list,
+                                                     interactive=interactive)
+
+            compare(new_config_path, sections_list, options_list_of_lists, values_list_of_lists)
+            shutil.copy(new_config_path, config_file_path)
+            files = os.listdir(os.path.dirname(new_config_path))
+            for file in files:
+                os.unlink(os.path.join(os.path.dirname(new_config_path), file))
+            os.rmdir(os.path.dirname(new_config_path))
+
+
+if __name__ == "__main__":
+    ### COMMAND LINE ARGUMENTS
+
+    # Init the command line arguments parser
+    arg_parser = argparse.ArgumentParser(description="Upgrade a .config file to match the .configTemplate format")
+
+    arg_parser.add_argument("config_file_path", nargs='?', default="./.config",
+                            help="Path to the .config file")
+
+    arg_parser.add_argument("--template", default="./.configTemplate",
+                        help="Path to .configTemplate (default: ./.configTemplate)")
+
+    arg_parser.add_argument("-i", "--interactive", default=False, action="store_true",
+                            help="Run interactively")
+
+
+
+    # Parse the command line arguments
+    cml_args = arg_parser.parse_args()
+
+    #########################
+
+    updateConfig(cml_args.config_file_path, cml_args.template, cml_args.interactive)
