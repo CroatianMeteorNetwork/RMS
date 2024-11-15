Fix byte/int8 overflow with numpy >= 2.0
Before numpy 2.0 a value of 255 was automatically converted
to -1 for int8/byte data type. Now it causes a overflow exception.

This change converts data to a larger data size before converting
it back to int8/byte. This should give same result.

RMS/Detection.py
@@ -530,7 +530,7 @@ def getLines(img_handle, k1, j1, time_slide, time_window_size, max_lines, max_wh
         w, h = img.shape[1], img.shape[0]
 
         # Convert the image to feed it into the KHT
-        img_flatten = (img.flatten().astype(np.byte)*255).astype(np.byte)
+        img_flatten = (img.flatten().astype(np.byte).astype(np.int16)*255).astype(np.byte)
         
         # Predefine the line output
         lines = np.empty((max_lines, 2), np.double)