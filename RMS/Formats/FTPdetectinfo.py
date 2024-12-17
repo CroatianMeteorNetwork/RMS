8f5fc27c3ae89fdf59c47d0be990b67685e8cdbf
Denis Vida <denis.vida@gmail.com>
2024-12-16 19:38:35 -0500

    added a catch for NaN background

--- RMS/Formats/FTPdetectinfo.py
+++ RMS/Formats/FTPdetectinfo.py
@@ -191,7 +191,7 @@ def writeFTPdetectinfo(meteor_list, ff_directory, file_name, cal_directory, cam_
 
                 # Set default values if necessary and limit the values to the allowed range
                 level = int(level) if level is not None else 1
-                background = int(background) if background is not None else 0
+                background = int(background) if (background is not None) and (not np.isnan(background)) else 0
                 snr = min(float(snr), 99.99) if snr is not None else 0.0
                 saturated_count = min(int(saturated_count), 999999) if saturated_count is not None else 0
 
