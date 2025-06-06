c9a8c5e4b8ed2ea678d3bbb345dca6ec7c8f1cc8
g7gpr <g7gpr@outlook.com>
2025-06-06 12:35:47 +0800

    Init reset_count

--- RMS/BufferedCapture.py
+++ RMS/BufferedCapture.py
@@ -169,6 +169,7 @@ class BufferedCapture(Process):
         # initialise last_calculated_fps to something sensible for edge case when only one sample is collected
         self.last_calculated_fps = self.config.fps
         self.last_calculated_fps_n = self.config.fps
+        self.reset_count = 0
 
     def startCapture(self, cameraID=0):
         """ Start capture using specified camera.
