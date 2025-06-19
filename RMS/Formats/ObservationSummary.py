f6b5769c2fd0d6ed56fa231d16d384ea02132bce
g7gpr <g7gpr@outlook.com>
2025-06-19 23:29:41 +0800

    Use full path for getting contemporary .config

--- RMS/Formats/ObservationSummary.py
+++ RMS/Formats/ObservationSummary.py
@@ -560,9 +560,9 @@ def estimateLens(fov_h):
 def getEphemTimesFromCaptureDirectory(config, capture_directory):
 
 
-    # capture_directory_full_path = os.path.join(config.data_dir, config.captured_dir, capture_directory)
+    capture_directory_full_path = os.path.join(config.data_dir, config.captured_dir, capture_directory)
     # print("Capture directory: {}".format(capture_directory_full_path))
-    night_config = parse(os.path.join(capture_directory,".config"))
+    night_config = parse(os.path.join(capture_directory_full_path,".config"))
     # print("Making a time from {}".format(capture_directory))
     capture_directory_start_time = filenameToDatetimeStr(os.path.basename(capture_directory))
     start_time, duration, end_time = getObservationDuration(night_config, capture_directory_start_time)
