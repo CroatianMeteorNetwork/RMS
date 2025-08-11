b78b34b604076798a4a1e521a004174e790bf0fb
Luc Busquin <133058544+Cybis320@users.noreply.github.com>
2025-08-10 23:48:58 -0400

    Switch to pip editable install and revamp RMS_Update.sh

--- setup.py
+++ setup.py
@@ -1,147 +1,94 @@
-from __future__ import print_function, division, absolute_import
-
-import os
+from __future__ import annotations
+from pathlib import Path
 import sys
-import subprocess
-import numpy
-
 from setuptools import setup, Extension, find_packages
-
 from Cython.Build import cythonize
 
-# Determine find_packages function to use depending on Python version
-find_packages_func = find_packages
+try:
+    import numpy as np
+    numpy_includes = [np.get_include()]
+except ModuleNotFoundError:
+    numpy_includes = []
 
-# If in Python 3.3 or later, load find_namespace_packages()
-if sys.version_info >= (3, 3):
-    from setuptools import find_namespace_packages
-    find_packages_func = find_namespace_packages
-
-
-kht_module = Extension("kht_module",
-                    sources = ["Native/Hough/kht.cpp",
-                               "Native/Hough/buffer_2d.cpp",
-                               "Native/Hough/eigen.cpp",
-                               "Native/Hough/linking.cpp",
-                               "Native/Hough/peak_detection.cpp",
-                               "Native/Hough/subdivision.cpp",
-                               "Native/Hough/voting.cpp"],
-                    include_dirs = ["Native/Hough/"],
-                    extra_compile_args=["-O3", "-Wall"])
-
-
-def isPackageInstalled(package_name):
-    """Check if a package is installed."""
+# --- bootstrap ---------------------------------------------------------------
+# This bootstrap code intercepts direct `python setup.py install` commands and 
+# redirects them to use pip in editable mode. This handles a critical transition
+# scenario:
+#
+# When RMS_Update.sh runs for the first time after this change:
+# 1. The script is already loaded in memory with the old `python setup.py install`
+# 2. Git pull updates the script to use `pip install -e .`
+# 3. But the in-memory version still executes the old command
+# 4. This bootstrap catches that and redirects it to the new pip command
+#
+# On subsequent runs, RMS_Update.sh will use the new command directly.
+#
+# How it works:
+# - Detects if setup.py was called with 'install' argument
+# - Excludes setuptools' internal flag '--old-and-unmanageable' to avoid loops
+# - Redirects to pip install with editable mode (-e), no deps, and no build isolation
+# - Exits with appropriate return code to maintain script compatibility
+#
+# This can be removed after a deprecation period once all users have migrated.
+from subprocess import check_call, CalledProcessError
+if "install" in sys.argv and "--old-and-unmanageable" not in sys.argv:
     try:
-        print("Checking if {:s} is installed...".format(package_name))
-        subprocess.check_call([sys.executable, '-c', 'import {:s}'.format(package_name)])
-        print("{:s} is already installed.".format(package_name))
-        return True
-    except subprocess.CalledProcessError:
-        print("{:s} is not installed.".format(package_name))
-        return False
+        check_call([sys.executable, "-m", "pip", "install",
+                    "-e", ".", "--no-deps", "--no-build-isolation"])
+    except CalledProcessError as exc:
+        sys.exit(exc.returncode)
+    sys.exit(0)
+# -----------------------------------------------------------------------------    
 
+# C/C++ extension
+kht_module = Extension(
+    "kht_module",
+    sources=[
+        "Native/Hough/kht.cpp",
+        "Native/Hough/buffer_2d.cpp",
+        "Native/Hough/eigen.cpp",
+        "Native/Hough/linking.cpp",
+        "Native/Hough/peak_detection.cpp",
+        "Native/Hough/subdivision.cpp",
+        "Native/Hough/voting.cpp",
+    ],
+    include_dirs=["Native/Hough/"],
+    extra_compile_args=["-O3", "-Wall"],
+)
 
-def attemptInstall(package):
-    """Attempt to install a package using pip."""
-    try:
-        print("Attempting to install {:s}...".format(package))
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
-        print("Successfully installed {:s}.".format(package))
-        return True
-    except subprocess.CalledProcessError:
-        print("Failed to install {:s}.".format(package))
-        return False
-
-
-
-# Check if TensorFlow is already installed
-if not isPackageInstalled('tensorflow'):
-    # Attempt to install tflite-runtime
-    if not attemptInstall('tflite-runtime'):
-        # If tflite-runtime fails, install TensorFlow
-        attemptInstall('tensorflow')
-
-# Read requirements file, filtering out comments and empty lines
-requirements = []
-with open('requirements.txt') as f:
-    for line in f:
-        # Remove whitespace from the beginning and end of the line
-        stripped_line = line.strip()
-
-        # Ignore comments and empty lines
-        if stripped_line and not stripped_line.startswith('#'):
-            requirements.append(stripped_line)
-
-
-### Add rawpy is running on Windows or Linux (not the Pi) ###
-# Check if running on Windows
-if 'win' in sys.platform and "darwin" not in sys.platform:
-    requirements.append("rawpy")
-
-# Check if running on Linux
-else:
-
-    # Check if running on the Pi
-    if any(arch in os.uname()[4].lower() for arch in ['arm', 'aarch']):
-        print("Not installing rawpy because it is not available on the Pi...")
-
-    else:
-        requirements.append("rawpy")
-
-### ###
-
-# Cython modules which will be compiled on setup
+# Cython extensions
 cython_modules = [
-    Extension('RMS.Astrometry.CyFunctions', sources=['RMS/Astrometry/CyFunctions.pyx'], \
-        include_dirs=[numpy.get_include()]),
-    Extension('RMS.Routines.BinImageCy', sources=['RMS/Routines/BinImageCy.pyx'], \
-        include_dirs=[numpy.get_include()]),
-    Extension('RMS.Routines.DynamicFTPCompressionCy', sources=['RMS/Routines/DynamicFTPCompressionCy.pyx'], \
-        include_dirs=[numpy.get_include()]),
-    Extension('RMS.Routines.Grouping3Dcy', sources=['RMS/Routines/Grouping3Dcy.pyx'], \
-        include_dirs=[numpy.get_include()]),
-    Extension('RMS.Routines.MorphCy', sources=['RMS/Routines/MorphCy.pyx'], \
-        include_dirs=[numpy.get_include()]),
-    Extension('RMS.CompressionCy', sources=['RMS/CompressionCy.pyx'], \
-        include_dirs=[numpy.get_include()]),
-    Extension('Utils.SaturationTools', sources=['Utils/SaturationTools.pyx'], \
-        include_dirs=[numpy.get_include()])
-    ]
+    Extension("RMS.Astrometry.CyFunctions", ["RMS/Astrometry/CyFunctions.pyx"], include_dirs=numpy_includes),
+    Extension("RMS.Routines.BinImageCy", ["RMS/Routines/BinImageCy.pyx"], include_dirs=numpy_includes),
+    Extension("RMS.Routines.DynamicFTPCompressionCy", ["RMS/Routines/DynamicFTPCompressionCy.pyx"], include_dirs=numpy_includes),
+    Extension("RMS.Routines.Grouping3Dcy", ["RMS/Routines/Grouping3Dcy.pyx"], include_dirs=numpy_includes),
+    Extension("RMS.Routines.MorphCy", ["RMS/Routines/MorphCy.pyx"], include_dirs=numpy_includes),
+    Extension("RMS.CompressionCy", ["RMS/CompressionCy.pyx"], include_dirs=numpy_includes),
+    Extension("Utils.SaturationTools", ["Utils/SaturationTools.pyx"], include_dirs=numpy_includes),
+]
 
+# Runtime requirements
+with open("requirements.txt") as fh:
+    requirements = [line.strip() for line in fh if line and not line.startswith("git+")]
 
-# Get all data files
-dir_path = os.path.split(os.path.abspath(__file__))[0]
-catalog_files = [
-    os.path.join('Catalogs', file_name) for file_name in os.listdir(os.path.join(dir_path, 'Catalogs'))
-    ]
-share_files = [
-    os.path.join('share', file_name) for file_name in os.listdir(os.path.join(dir_path, 'share')) 
-        if os.path.isfile(os.path.join(dir_path, 'share', file_name))
-        ]
-platepar_templates = [
-    os.path.join('share', 'platepar_templates', file_name) 
-        for file_name in os.listdir(os.path.join(dir_path, 'share', 'platepar_templates')) 
-        if os.path.isfile(os.path.join(dir_path, 'share', 'platepar_templates', file_name))
-        ]
+# Data files
+base = Path(__file__).resolve().parent
+catalog_files = [str(p.relative_to(base)) for p in (base / "Catalogs").glob("*")]
+share_files   = [str(p.relative_to(base)) for p in (base / "share").glob("*") if p.is_file()]
+plate_files   = [str(p.relative_to(base)) for p in (base / "share/platepar_templates").glob("*")]
 
-
-# Print the requirements to be installed
-print("Requirements to be installed:")
-for req in requirements:
-    print(" - {}".format(req))
-
-
-setup (name = "RMS",
-        version = "0.1",
-        description = "Raspberry Pi Meteor Station",
-        setup_requires=["numpy", 
-        # Setuptools 18.0 properly handles Cython extensions.
-            'setuptools>=18.0',
-            'cython'],
-        install_requires=requirements,
-        data_files=[('Catalogs', catalog_files), ('share', share_files), ('share/platepar_templates', platepar_templates)],
-        ext_modules = [kht_module] + cythonize(cython_modules),
-        packages=find_packages_func(),
-        include_package_data=True
-        )
+setup(
+    name="RMS",
+    version="0.1",
+    description="Raspberry Pi Meteor Station",
+    packages=find_packages(),
+    install_requires=requirements,
+    data_files=[
+        ("Catalogs", catalog_files),
+        ("share", share_files),
+        ("share/platepar_templates", plate_files),
+    ],
+    ext_modules=[kht_module] + cythonize(cython_modules),
+    include_dirs=numpy_includes,
+    include_package_data=True,
+)
