Add out-of-bounds penalty to ASTRA configuration and fix percentile calculation in masked cropping

Utils/ASTRA.py
@@ -99,7 +99,8 @@ class ASTRA:
             "ftol_iter": ftol_iter,
             "bh_strategy": bh_strategy,
             "vh_strategy": vh_strategy,
-            "explorative_coeff": explorative_coeff
+            "explorative_coeff": explorative_coeff,
+            "oob_penalty" : 1e6
         }
 
         # Settings and padding coefficients for cropping
@@ -1015,7 +1016,7 @@ class ASTRA:
         std_ave = np.std(masked_avebk[masked_avebk > 0])
 
         # Crop the masked frame to the percentile threshold defined by P_thresh
-        masked_cropped[masked_cropped < np.ma.percentile(masked_cropped, float(self.astra_config['astra']['photom_thresh']))] = 0
+        masked_cropped[masked_cropped < np.nanpercentile(masked_cropped, float(self.astra_config['astra']['photom_thresh']))] = 0
 
         # Count the non-zero values in the masked cropped frame, and calculate the level sum
         nonzero_count = np.count_nonzero(masked_cropped)
@@ -2070,7 +2071,7 @@ class ASTRA:
         # Mask cropped frame with fit image to remove the background
         masked_cropped = fit_img * cropped_frame
 
-        masked_cropped[masked_cropped < np.ma.percentile(masked_cropped, float(self.astra_config['astra']['photom_thresh']))] = 0
+        masked_cropped[masked_cropped < np.nanpercentile(masked_cropped, float(self.astra_config['astra']['photom_thresh']))] = 0
 
         # binarize mask_cropped
         masked_cropped[masked_cropped > 0] = 1

Utils/SkyFit2.py
@@ -4183,10 +4183,17 @@ class PlateTool(QtWidgets.QMainWindow):
         temp_curr_frame = self.img_handle.current_frame
         self.img_handle.setFrame(0)  # Reset to the first frame
         frame_count = sum(1 for name in os.listdir(self.dir_path) if 'dump' in name)
-        frames = np.zeros((frame_count, *self.img_handle.loadFrame().shape), dtype=np.float32)
+
+        # Load the first frame
+        frame = self.img_handle.loadFrame()
+
+        # Init a numpy array with the correct size and type
+        frames = np.zeros((frame_count, frame.shape[0], frame.shape[1]), dtype=frame.dtype)
+
         for i in range(frame_count):
             frames[i] = self.img_handle.loadFrame()
             self.img_handle.nextFrame()
+            
         self.img_handle.setFrame(temp_curr_frame)  # Reset to the original frame
         # Load all times
         pick_frame_indices = np.array(