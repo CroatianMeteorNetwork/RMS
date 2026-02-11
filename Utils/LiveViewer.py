8b668196e6304890131c580850c97a032b7d021c
g7gpr <g7gpr@outlook.com>
2026-02-11 09:27:14 +0000

    Rewrite

--- Utils/LiveViewer.py
+++ Utils/LiveViewer.py
@@ -409,7 +409,8 @@ class LiveViewer(multiprocessing.Process):
             elif not self.capturing:
                 archived_dir_path = os.path.join(self.config.data_dir, self.config.archived_dir)
                 archived_dir_list = os.listdir(archived_dir_path)
-                self.dir_path = sorted(fnmatch.filter(archived_dir_list, f"{self.config.stationID.upper()}_*_*_*"))[-1]
+                latest_archive_dir = sorted(fnmatch.filter(archived_dir_list, f"{self.config.stationID.upper()}_*_*_*"))[-1]
+                self.dir_path = os.path.join(archived_dir_path, latest_archive_dir)
 
                 self.startSlideshow()
 
