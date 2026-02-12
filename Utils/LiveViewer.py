389c7395b331722db377ee3501b0e7a63797dd66
g7gpr <g7gpr@outlook.com>
2026-02-12 12:41:42 +0000

    Monitor FramesFiles from the command line

--- Utils/LiveViewer.py
+++ Utils/LiveViewer.py
@@ -472,6 +472,8 @@ class LiveViewer(multiprocessing.Process):
         if self.dir_path is not None:
             if os.path.exists(self.dir_path):
                 print("Path exists")
+                print(os.path.expanduser(self.dir_path))
+                print(os.path.expanduser(os.path.join(self.config.data_dir, self.config.frame_dir)))
                 if os.path.expanduser(self.dir_path) == (os.path.expanduser(os.path.join(self.config.data_dir, self.config.frame_dir))):
                     print("Paths matched")
                     self.monitorFramesDirOnly = True
