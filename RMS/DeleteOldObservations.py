fd61aa767ba9acdf2ac1e4ce318ee5ce99b126f7
g7gpr <g7gpr@outlook.com>
2026-03-05 13:18:46 +0000

    Add whitespace

--- RMS/DeleteOldObservations.py
+++ RMS/DeleteOldObservations.py
@@ -113,11 +113,11 @@ def quotaReport(capt_dir_quota, config, after=False):
     rep += ("                       frames files : {:7.02f}GB\n".format(frames_files_used_space))
     rep += ("                         time files : {:7.02f}GB\n".format(time_files_used_space))
     rep += ("                        video files : {:7.02f}GB\n".format(video_files_used_space))
-    rep += ("                                        ----------------------\n")
+    rep += ("                                        ----------------------\n\n")
     rep += ("       total for continuous capture : {:7.02f}GB {:7.02f}GB {:3.0f}%\n".format(continuous_capture_used_space, config.continuous_capture_quota, continuous_capture_pc))
     rep += ("                          bz2 files : {:7.02f}GB {:7.02f}GB {:3.0f}%\n".format(sizeBz2Files(config), config.bz2_files_quota, bz2_files_pc))
     rep += ("               archived directories : {:7.02f}GB {:7.02f}GB {:3.0f}%\n".format(sizeArchivedDirs(config), config.arch_dir_quota, arch_dir_pc))
-    rep += ("                                        ----------------------\n")
+    rep += ("                                        ----------------------\n\n")
     rep += (" bz2 files and archived directories : {:7.02f}GB {:7.02f}GB {:3.0f}%\n".format(usedSpace(archived_dir), config.arch_dir_quota + config.bz2_files_quota, total_arch_pc))
 
     rep += ("               captured directories : {:7.02f}GB {:7.02f}GB {:3.0f}%\n".format(usedSpace(captured_dir), capt_dir_quota, captured_dir_pc))
